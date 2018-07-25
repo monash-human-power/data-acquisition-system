e89a11fbe3d3b073bdd5ab4c9d05ff670a2dc91e
khanguslee <angus.lee809@gmail.com>
2018-07-25 17:51:29 +1000

    Get current date and time

--- Raspi/DAS.py
+++ Raspi/DAS.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import requests
 
 ser = serial.Serial(
 	'/dev/serial0',
@@ -9,10 +10,17 @@ ser = serial.Serial(
 
 while True:
 	# Check if there is anything in the buffer
-	if ser.in_waiting > 0:
+	serial_in_waiting = ser.in_waiting
+	if serial_in_waiting > 0:
 		# Read and print the contents of the buffer
-		serial_output = ser.read(ser.in_waiting)
+		serial_output = ser.read(serial_in_waiting)
 		output = serial_output.decode('utf-8')
+
+		# Tell server to create a new csv file due to start of recording data
+		if (output == 'start'):
+			current_date_time = time.strftime('%Y_%m_%d_%H_%M_%S')
+			filename = 'data_' + current_date_time
+			print(filename)
 		print(output)
 
 	# Introduce some sort of delay so that the buffer can be cleared in time
