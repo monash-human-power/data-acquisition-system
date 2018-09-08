78b5c33aee799c6b03a314596427a87356ac3cd4
khanguslee <angus.lee809@gmail.com>
2018-09-08 17:22:48 +1000

    Experiment with promises

--- Raspi/DAS.js
+++ Raspi/DAS.js
@@ -1,6 +1,6 @@
 /* Set up modules */
 // Set up server connection
-const request = require('request');
+const request = require('request-promise');
 const DAS_SERVER_ADDR = "http://127.0.0.1:5000";
 var IS_SERVER_CONNECTED = false;
 
@@ -29,6 +29,7 @@ serialPort.pipe(parser);
 /* Start of main code */
 // Check if server is connected
 function check_if_server_online() {
+	// Sends a GET request to the /server/status endpoint
 	request.get(DAS_SERVER_ADDR + '/server/status', {timeout: 1000}, (error, res, body) => {
 		if (error) {
 			console.log(error);
@@ -45,7 +46,6 @@ function check_if_server_online() {
 			return false;
 		}
 	});
-
 }
 
 // Check if server is online every 1.5 seconds
@@ -64,11 +64,26 @@ function main(){
 	})
 }
 
+function create_file_name() {
+	return request.get(DAS_SERVER_ADDR + '/files', (error, response, body) => {
+		if (error) {
+			console.log(error);
+			return false;
+		}
+	})
+}
+
 serialPort.on("open", () => {
 	console.log('Port opened with Teensy');
 	
 	// Server and Teensy need to be both connected to receive data
 	parser.on('data', (data) => {
 		console.log(data);
+		if (data == 'start') {
+			create_file_name()
+				.then( body => {
+					console.log(body);
+				})
+		}
 	});
 })
