e72e17bd7d97a9b151329491f183809e86591abd
Kuan Hou Lee <angus.lee809@gmail.com>
2019-03-18 16:50:28 +1100

    Add arguments

--- Raspi/Test/mqtt_test.py
+++ Raspi/Test/mqtt_test.py
@@ -1,27 +1,42 @@
 import sys
 import time
+import random
+import argparse
 import paho.mqtt.client as mqtt
 
-# TODO: Take in time and length of time
+# Arguments
+parser = argparse.ArgumentParser(description='DAS MQTT python script', add_help=True)
+parser.add_argument('-t', '--time', action='store', type=int, default=1, help="length of time to send data")
+parser.add_argument('-r', '--rate', action='store', type=float, default=0.5, help="rate of data in seconds")
+
+args = parser.parse_args()
 def start_publishing(client):
     start_time = round(time.time(),2)
     print("start")
     client.publish("start")
+    gps_speed = 0
     while(True):
         current_time = round(time.time(), 2)
         total_time = round(current_time - start_time, 2)
-        data = "gps=1&gps_location=00&gps_course=00&gps_speed=00&gps_satellites=00"
+
+        # Generate data message
+        data = "gps=1&gps_location=00&gps_course=00&"
+        gps_speed = random.random() * (total_time) + gps_speed
+        data += "gps_speed=" + str(gps_speed)
+        data += "&gps_satellites=00"
         data += "&aX=00.0000&aY=00.0000&aZ=00.0000&gX=00.0000&"
         data += "gY=00.0000&gZ=00.0000"
         data += "&thermoC=25.00&thermoF=25.00"
         data += "&pot=100"
         data += "&filename=test.csv"
         data += "&time=" + str(total_time * 1000)
-        data += "&power=100&cadence=100"
+        data += "&power=" + str(random.randint(0,150))
+        data += "&cadence=" + str(random.randint(0,150))
         client.publish("data", data)
         print(data)
-        time.sleep(0.5)
-        if (total_time > 5):
+
+        time.sleep(args.rate)
+        if (total_time >= args.time):
             break
     print("stop")
     client.publish("stop")
