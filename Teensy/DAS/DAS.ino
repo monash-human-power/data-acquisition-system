6a5725e468067bd52c7068d7f198d92435cac553
khanguslee <angus.lee809@gmail.com>
2019-03-22 02:04:40 +1100

    Add reed switch data

--- Teensy/DAS/DAS.ino
+++ Teensy/DAS/DAS.ino
@@ -28,6 +28,7 @@ volatile int button_state = 0;
 #endif
 
 // Pin Values
+static const int REED_SWITCH_PIN = 2;
 static const int BUTTON_PIN = 5;
 static const int RECORD_LED = 13;
 static const int STATUS_LED = 12, WARNING_LED = 11 , ERROR_LED = 4;
@@ -36,6 +37,16 @@ static const int GPS_RXPin = 7, GPS_TXPin = 8;
 #define RPISERIAL Serial2 // Pin 9 (RX) & PIN 10 (TX) for Teensy LC
 //const int SDchipSelect = 10;
 const int LMS6DS3chipSelect = 6;
+
+// Set up reed switch
+volatile unsigned long PREV_REED_SWITCH_TIME = 0;
+volatile unsigned long REED_SWITCH_COUNTER = 0;
+volatile unsigned long PREV_REED_SWITCH_COUNT = 0;
+volatile float VELOCITY = 0, DISTANCE = 0;
+
+// 700C Rims + 33mm tyre
+const float WHEEL_DIAMETER = 0.622 + (0.033 * 2);
+IntervalTimer reedSwitchTimer;
 
 // Set up Accelerometer
 LSM6DS3 myIMU(SPI_MODE, LMS6DS3chipSelect);
@@ -52,6 +63,12 @@ int rpi_baud_rate = 500000;   // Set baud rate to 500000
 
 void setup()
 {
+  // Set up Reed Switch
+  pinMode(REED_SWITCH_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), reedSwitchInterruptHandler, FALLING); // interrrupt 1 is data ready
+  // Check if stopped every 3 seconds
+  reedSwitchTimer.begin(reedSwitchChecker, 3 * 1000000);
+
   // Set Up Button Pin
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
@@ -213,7 +230,18 @@ void loop()
       DEBUG_PRINTLN(pot_data);
       DEBUG_PRINTLN("");
       output_data += "&pot=" + pot_data;
-      
+
+      /* Reed Switch */
+      DEBUG_PRINTLN("REED SWITCH DATA:");
+      DEBUG_PRINTLN("Velocity:");
+      DEBUG_PRINT_DEC(VELOCITY, 4);
+      DEBUG_PRINTLN("");
+      DEBUG_PRINTLN("Distance:");
+      DEBUG_PRINT_DEC(DISTANCE, 4);
+      DEBUG_PRINTLN("");
+      output_data += "&reed_velocity=" + String(VELOCITY, 4);
+      output_data += "&reed_distance=" + String(DISTANCE, 4);
+
       DEBUG_PRINTLN(output_data);
       writeStringToRPi(output_data);
     }     
@@ -316,5 +344,47 @@ void switchInterruptHandler()
     DEBUG_PRINTLN("OFF");
     writeStringToRPi("stop");
   }
+}
+
+void reedSwitchInterruptHandler() {
+  if(is_recording != 1) {
+    return;
+  }
+  // Get start time
+  if(PREV_REED_SWITCH_TIME == 0) {
+    PREV_REED_SWITCH_TIME = millis();
+    VELOCITY = 0;
+    return;
+  }
+  unsigned long current_time = millis();
+  unsigned long total_time = current_time - PREV_REED_SWITCH_TIME;
+  // 50ms means we have a maximum velocity of 180km/h with a wheel diameter of 0.8
+  if(total_time <= 50) {
+    return;
+  }
+
+  // Count how many times wheel rotates
+  REED_SWITCH_COUNTER = REED_SWITCH_COUNTER + 1;
+
+  // Calculate velocity and distance
+  VELOCITY = (1/(float)total_time) * PI * WHEEL_DIAMETER * 3600;
+  DISTANCE = REED_SWITCH_COUNTER * WHEEL_DIAMETER;
+  PREV_REED_SWITCH_TIME = current_time;
+}
+
+// Check if the bike has stopped
+void reedSwitchChecker() {
+  if(is_recording != 1) {
+    return;
+  }
+  if(PREV_REED_SWITCH_COUNT != REED_SWITCH_COUNTER) {
+    PREV_REED_SWITCH_COUNT = REED_SWITCH_COUNTER;
+  } else {
+    // Stopped!
+    noInterrupts();
+    PREV_REED_SWITCH_TIME = 0;
+    VELOCITY = 0;
+    interrupts();
+  }  
 }
 
