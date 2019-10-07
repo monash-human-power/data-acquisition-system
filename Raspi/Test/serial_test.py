e64d3de68dad0515ecb36c84e8a884fde15bcc62
Christopher Hall <hallgchris@gmail.com>
2019-10-08 07:29:56 +1100

    Publish filename on MQTT and don't send data that is created by DAS.js

--- Raspi/Test/serial_test.py
+++ Raspi/Test/serial_test.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import paho.mqtt.client as mqtt
 
 from test_data_generator import send_csv_data, send_fake_data
 
@@ -7,8 +8,12 @@ from test_data_generator import send_csv_data, send_fake_data
 parser = argparse.ArgumentParser(description='DAS serial test script', add_help=True)
 parser.add_argument('-t', '--time', action='store', type=int, default=1, help="length of time to send data")
 parser.add_argument('-r', '--rate', action='store', type=float, default=0.5, help="rate of data in seconds")
+parser.add_argument('--host', action='store', type=str, default="localhost", help="address of the MQTT broker")
 parser.add_argument('-f', '--file', action='store', type=str, help="The csv file to replay. If not specified, makes up data.")
 parser.add_argument('-j', '--jump', action='store', type=int, default=0, help="Starts replaying from a specified time (in seconds)")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code " + str(rc))
 
 def run(args):
     # Create pseudo-terminal pair
@@ -21,15 +26,23 @@ def run(args):
     # Allow user to start DAS.js before starting so that it recieves the 'start' topic
     input("Run 'DAS.js -p {}' then press any key".format(os.ttyname(slave)))
 
+    # Connect to MQTT
+    broker_address = args.host
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(broker_address)
+    # Publish to filename topic so DAS.js starts rebroadcasting stuff
+    client.publish("filename", "serial_test")
+
     # Function to write the data to serial
     send_data_func = lambda data: os.write(master, (data + "\n").encode())
 
     send_data_func("start")
 
     if args.file is None:
-        send_fake_data(send_data_func, args.time, args.rate)
+        send_fake_data(send_data_func, args.time, args.rate, immitate_teensy=True)
     else:
-        send_csv_data(send_data_func, args.file, args.jump)
+        send_csv_data(send_data_func, args.file, args.jump, immitate_teensy=True)
 
     send_data_func("stop")
 
--- Raspi/Test/test_data_generator.py
+++ Raspi/Test/test_data_generator.py
@@ -3,8 +3,10 @@ import os
 import random
 import time
 
-def send_fake_data(send_data_func, duration, rate):
-    """ Send artificial data over MQTT if no file is specified. Sends [rate] per second for [duration] seconds """
+def send_fake_data(send_data_func, duration, rate, immitate_teensy=False):
+    """ Send artificial data over MQTT if no file is specified. Sends [rate] per second for [duration] seconds
+        Some fields are filled in by DAS.js, so if data will go through DAS.js (i.e. serial_test.py)
+        we don't want to send that data """
     start_time = round(time.time(), 2)
     gps_speed = 0
     while True:
@@ -20,12 +22,13 @@ def send_fake_data(send_data_func, duration, rate):
         data += "gY=00.0000&gZ=00.0000"
         data += "&thermoC=25.00&thermoF=25.00"
         data += "&pot=100"
-        data += "&filename=test.csv"
-        data += "&time=" + str(total_time * 1000)
-        data += "&power=" + str(random.randint(0, 150))
-        data += "&cadence=" + str(random.randint(0, 150))
         data += "&reed_velocity=" + str(random.randint(0, 150))
         data += "&reed_distance=" + str(random.randint(0, 150))
+        if not immitate_teensy:
+            data += "&filename=test.csv"
+            data += "&time=" + str(total_time * 1000)
+            data += "&power=" + str(random.randint(0, 150))
+            data += "&cadence=" + str(random.randint(0, 150))
 
         send_data_func(data)
         print(data)
@@ -34,8 +37,10 @@ def send_fake_data(send_data_func, duration, rate):
         if total_time >= duration:
             break
 
-def send_csv_data(send_data_func, csv_path, jump):
-    """ Replays a ride recorded to a csv located at csv_path. Starts from [jump] seconds """
+def send_csv_data(send_data_func, csv_path, jump, immitate_teensy=False):
+    """ Replays a ride recorded to a csv located at csv_path. Starts from [jump] seconds
+        Some fields are filled in by DAS.js, so if data will go through DAS.js (i.e. serial_test.py)
+        we don't want to send that data """
     with open(csv_path) as csv_data:
         reader = csv.DictReader(csv_data)
 
@@ -55,16 +60,18 @@ def send_csv_data(send_data_func, csv_path, jump):
             prev_time = row_time
 
             # Create data to send via MQTT
-            data = "time={}".format(row_time)
-            data += "&gps={}&gps_course={}&gps_speed={}&gps_satellites={}" \
+            data = "&gps={}&gps_course={}&gps_speed={}&gps_satellites={}" \
                     .format(line["gps"], line["gps_course"], line["gps_speed"], line["gps_satellites"])
             data += "&aX={}&aY={}&aZ={}".format(line["aX"], line["aY"], line["aZ"])
             data += "&gX={}&gY={}&gZ={}".format(line["gX"], line["gY"], line["gZ"])
             data += "&thermoC={}&thermoF={}".format(line["thermoC"], line["thermoF"])
             data += "&pot={}".format(line["pot"])
-            data += "&power={}&cadence={}".format(line["power"], line["cadence"])
             data += "&reed_velocity={}&reed_distance={}".format(line["reed_velocity"], line["reed_distance"])
-            data += "&filename={}".format(os.path.basename(csv_path))
+
+            if not immitate_teensy:
+                data += "&filename={}".format(os.path.basename(csv_path))
+                data += "time={}".format(row_time)
+                data += "&power={}&cadence={}".format(line["power"], line["cadence"])
 
             # Mid July 2019 - field gps_location split into three separate columns
             if "gps_location" in reader.fieldnames:
