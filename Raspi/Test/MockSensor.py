a050e18f5ed40b36a1c3ec567ba9ce373fe853ee
Blake <bhay0010@student.monash.edu>
2020-01-10 15:23:32 +1100

    Add init to MockSensor Class

--- Raspi/Test/MockSensor.py
+++ Raspi/Test/MockSensor.py
@@ -1,55 +1,35 @@
-class Sensors:
 
-    # number of decimal places for the random generated val
-    decimals = 2
-    # percent range for the rangomly generated value
-    percent_range = 0.05
 
-    def CO2(val=325):
-        # value in ppm (normal background conentration 250-400ppm)
-        return round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)
+class MockSensor:
+    """ Base class to make a mock sensor that produces random data"""
 
-    def humidity(val=75):
-        # value as a percentage of humidity
-        return round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)
+    def __init__(self, *ave_val, percent_range=0.05, decimals=2):
+        """
+        ave_val:        Either a single val that sets the average value for the
+                        sensor or and array of subvals formatted as
+                        (sub_value_name, ave_val). Where sub_value_name is the
+                        name that will be used in the outputed dictionary and
+                        the ave_val is similar to a single val.
+        percent_range:  Percent range for the randomly generated value
+        decimals:       Number of decimal places for the random generated val
+        """
 
-    def temperature(val=20):
-        # value in degrees celcius
-        return round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)
+        self.ave_val = ave_val
+        self.percent_range = percent_range
+        self.decimals = decimals
 
-    def accelerometer(val=5):
-        # value in m/s^2 for all axis stored in a dictionary
-        return {
-        "x": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "y": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "z": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)}
+        if len(ave_val) == 1:
+            if type(ave_val[0]) != int and type(ave_val[0]) != float:
+                raise ValueError("""Single values must be either and int or a
+                                 float""")
+            self.single_val = True
 
-    def gyroscope(val=90):
-        # value in degrees for all axis stored in a dictionary
-        return {
-        "x": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "y": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "z": round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)}
+        if len(ave_val) != 1:
+            for sub_val in ave_val:
+                if type(sub_val) != tuple:
+                    raise ValueError("""Sub values must be stored as a tuple
+                                    in the form (sub_value_name, ave_val)""")
+class MockSensor:
+        ave_val:        Either a single val that sets the average value for the
 
-    def battery(val=80):
-        # value as a percentage of the battery
-        return round(val + val*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)
-
-    def reedSwitch(val=True):
-        # value as either true or false
-        return val
-
-    def GPS(latLngVal=20, speedVal=50):
-        # returns the latatude, longatude and the GPS speed of the bike stored in a dictionary
-        return {
-        "lat": round(latLngVal + latLngVal*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "lng": round(latLngVal + latLngVal*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals),
-        "speed": round(speedVal + speedVal*random.uniform(-Sensors.percent_range, Sensors.percent_range), Sensors.decimals)}
-
-    def steering(val=0):
-        # value in degrees for the position of the steering stem
-        return round(val + random.uniform(-90, 90), Sensors.decimals)
-
-    def time():
-        # return the current time for the sensor as Unix Epoch time
-        return time.time()
+            self.single_val = False
