5bf63d6109ddcc93e00332dfbdcade712dab7318
Blake <bhay0010@student.monash.edu>
2020-01-10 19:45:02 +1100

    Swap type check for "isinstance()"

--- Raspi/Test/MockSensor.py
+++ Raspi/Test/MockSensor.py
@@ -20,14 +20,15 @@ class MockSensor:
         self.decimals = decimals
 
         if len(ave_val) == 1:
-            if type(ave_val[0]) != int and type(ave_val[0]) != float:
+            if (not isinstance(ave_val[0], int)
+               and not isinstance(ave_val[0], float)):
                 raise ValueError("""Single values must be either and int or a
                                  float""")
             self.single_val = True
 
         if len(ave_val) != 1:
             for sub_val in ave_val:
-                if type(sub_val) != tuple:
+                if not isinstance(sub_val, tuple):
                     raise ValueError("""Sub values must be stored as a tuple
                                     in the form (sub_value_name, ave_val)""")
             self.single_val = False
