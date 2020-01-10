f4ae8303c25c7fd4e94ab8f8482bd876710780e2
Blake <bhay0010@student.monash.edu>
2020-01-10 17:04:18 +1100

    Refactor test code with the new MockSensor Class

--- Raspi/Test/mock_wireless_module.py
+++ Raspi/Test/mock_wireless_module.py
@@ -57,73 +57,81 @@ def send_fake_data(client, duration, rate, sensor_ID):
     while True:
         current_time = round(time.time(), 2)
         total_time = round(current_time - start_time, 2)
-
         print()     # Newline for clarity
 
         # Wireless Sensor 1 (Middle)
         if sensor_ID == 1 or sensor_ID is None:
+            sensor_data = {
+                            "filename":         "test-file.csv",
+                            "temperature":      s_temperature.get_value(),
+                            "humidity":         s_humidity.get_value(),
+                            "steeringAngle":    s_steering_angle.get_value()
+                          }
+            battery_data = {"percentage": s_battery.get_value()}
 
-            # generate JSON data and publish it over MQTT
-            data = {
-                "CO2": Sensors.CO2(),
-                "temperature": Sensors.temperature(),
-                "humidity": Sensors.humidity(),
-                "accelerometer": Sensors.accelerometer(),
-                "gyroscope": Sensors.gyroscope(),
-                "battery": Sensors.battery(),
-                "time": Sensors.time()}
+            sensor_topic = "v3/wireless-sensor/1/data"
+            battery_topic = "v3/wireless-sensor/1/battery"
 
-            JSON_data = json.dumps(data)
-            pub_path = "v3/wireless-sensor/1/data"
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
 
-            client.publish(pub_path, JSON_data)
+        # Wireless Sensor 2 (Back)
         if sensor_ID == 2 or sensor_ID is None:
+            sensor_data = {
+                    "filename":         "test-file.csv",
+                    "co2":              s_c02.get_value(),
+                    "temperature":      s_temperature.get_value(),
+                    "humidity":         s_humidity.get_value(),
+                    "accelerometer":    s_accelerometer.get_value(),
+                    "gyroscope":        s_gyroscope.get_value()
+                   }
+            battery_data = {"percentage": s_battery.get_value()}
 
-        # Wireless Sensor 2 (client, Back)
+            sensor_topic = "v3/wireless-sensor/2/data"
             battery_topic = "v3/wireless-sensor/2/battery"
 
-            # generate JSON data and publish it over MQTT
-            data = {
-                "CO2": Sensors.CO2(),
-                "reedSwitch": Sensors.reedSwitch(),
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
+
+        # Wireless Sensor 3 (Front)
         if sensor_ID == 3 or sensor_ID is None:
-                "temperature": Sensors.temperature(),
-                "battery": Sensors.battery(),
-                "time": Sensors.time()}
+            sensor_data = {
+                    "filename":         "test-file.csv",
+                    "co2":              s_c02.get_value(),
+                    "reedVelocity":     s_reed_velocity.get_value(),
+                    "gps":              s_GPS.get_value(),
+                   }
+            battery_data = {"percentage": s_battery.get_value()}
 
-            JSON_data = json.dumps(data)
-            pub_path = "v3/wireless-sensor/2/data"
+            sensor_topic = "v3/wireless-sensor/2/data"
+            battery_topic = "v3/wireless-sensor/2/battery"
 
-            client.publish(pub_path, JSON_data)
-            print(pub_path, "--> ", JSON_data)
-
-        # Wireless Sensor 3 (client, Front)
-        if sensorID == 3 or sensorID is None:
-
-            # generate JSON data and publish it over MQTT
-            data = {
-                "steering": Sensors.steering(),
-                "temperature": Sensors.temperature(),
-                "humidity": Sensors.humidity(),
-                "battery": Sensors.battery(),
-                "time": Sensors.time()}
-
-            JSON_data = json.dumps(data)
-            pub_path = "v3/wireless-sensor/3/data"
-
-            client.publish(pub_path, JSON_data)
-            print(pub_path, "--> ", JSON_data)
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
 
         time.sleep(1/rate)
         if total_time >= duration:
             break
 
 
+def publish(client, topic, data):
+    """
+    Publishes python dict data to a specific topic in JSON and prints it out
+    client: MQTT client object
+    topic:  MQTT topic eg. '/v3/wireless-module/<id>/start'
+    data:   python dict containin the data to be published on the topic
+    """
+    # generate JSON from the python dict
+    JSON_data = json.dumps(data)
+
+    # publish the data over MQTT
+    client.publish(topic, JSON_data)
+    print(topic, "--> ", JSON_data)
+
+
 def start_publishing(client, args):
     print("publishing started...")
-
     send_fake_data(client, args.time, args.rate, args.id)
-
     print("publishing finished")
 
 
