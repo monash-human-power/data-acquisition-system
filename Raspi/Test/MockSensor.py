72570f15ce0c67d004ae2570ee12eb40cf2628af
Blake <bhay0010@student.monash.edu>
2020-01-13 13:38:37 +1100

    Change 'ave_val' to 'average_value'

--- Raspi/Test/MockSensor.py
+++ Raspi/Test/MockSensor.py
@@ -4,33 +4,35 @@ import random
 class MockSensor:
     """ Base class to make a mock sensor that produces random data"""
 
-    def __init__(self, *ave_val, percent_range=0.05, decimals=2):
+    def __init__(self, *average_value, percent_range=0.05, decimals=2):
         """
-        ave_val:        Either a single val that sets the average value for the
+        average_value:  Either a single val that sets the average value for the
                         sensor or an array of subvals formatted as
-                        (sub_value_name, ave_val). Where sub_value_name is the
-                        name that will be used in the outputted dictionary and
-                        the ave_val is similar to a single val.
+                        (sub_value_name, average_value). Where sub_value_name
+                        is the name that will be used in the outputted
+                        dictionary and the average_value is similar to a single
+                        val.
         percent_range:  Percent range for the randomly generated value
         decimals:       Number of decimal places for the random generated val
         """
 
-        self.ave_val = ave_val
+        self.average_value = average_value
         self.percent_range = percent_range
         self.decimals = decimals
 
-        if len(ave_val) == 1:
-            if (not isinstance(ave_val[0], int)
-               and not isinstance(ave_val[0], float)):
+        if len(average_value) == 1:
+            if (not isinstance(average_value[0], int)
+               and not isinstance(average_value[0], float)):
                 raise TypeError("""Single values must be either and int or a
                                  float""")
             self.single_val = True
 
         else:
-            for sub_val in ave_val:
+            for sub_val in average_value:
                 if not isinstance(sub_val, tuple):
                     raise TypeError("""Sub values must be stored as a tuple
-                                    in the form (sub_value_name, ave_val)""")
+                                    in the form (sub_value_name, average_value)
+                                    """)
             self.single_val = False
 
     def get_value(self):
@@ -38,21 +40,21 @@ class MockSensor:
             for mutliple sub values"""
 
         if self.single_val:
-            return self.gen_single_value(self.ave_val[0])
+            return self.gen_single_value(self.average_value[0])
 
         else:
             sensor_dict = {}
-            for sub_val in self.ave_val:
+            for sub_val in self.average_value:
                 sub_val_name = sub_val[0]
-                sub_ave_val = sub_val[1]
-                sensor_dict[sub_val_name] = self.gen_single_value(sub_ave_val)
+                sub_average_value = sub_val[1]
+                sensor_dict[sub_val_name] = self.gen_single_value(sub_average_value)
 
             return sensor_dict
 
-    def gen_single_value(self, ave_val):
+    def gen_single_value(self, average_value):
         """ Generates a single value given an average value"""
-        sensor_val = ave_val
-        sensor_val += ave_val * random.uniform(-self.percent_range,
-                                               self.percent_range)
+        sensor_val = average_value
+        sensor_val += average_value * random.uniform(-self.percent_range,
+                                                     self.percent_range)
         sensor_val = round(sensor_val, self.decimals)
         return sensor_val
