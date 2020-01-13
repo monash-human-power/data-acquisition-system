3c197ff29ece2564d2f3fb321afc158dd791ae86
Blake <bhay0010@student.monash.edu>
2020-01-13 14:24:40 +1100

    Add steps needed to complete

--- Raspi/Test/mqtt_wireless_log.py
+++ Raspi/Test/mqtt_wireless_log.py
@@ -2,3 +2,13 @@ import csv
 import json
 import argparse
 import paho.mqtt.client as mqtt
+
+"""
+1. Subscribe to the chanels
+2. Capture and decode the JSON
+3. Convert the json to csv
+4. append the data to the csv file
+
+# NOTE: The battery will be implemented later. Just do the key data first.
+
+"""
