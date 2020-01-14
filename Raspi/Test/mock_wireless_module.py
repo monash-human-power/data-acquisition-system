e7a1317357f8567ede55c12fdc156a0b0b6a2b41
Blake <bhay0010@student.monash.edu>
2020-01-14 14:39:46 +1100

    Create mock wireless module test script (#30)
    Allows for mock wireless data to be sent from the DAS in order to test logging and whether MQTT is working correctly.

---
+++ Raspi/Test/mock_wireless_module.py
@@ -0,0 +1,197 @@
+import time
+import argparse
+import json
+import paho.mqtt.client as mqtt
+from MockSensor import MockSensor
+
+parser = argparse.ArgumentParser(
+    description='MQTT wireless sensor test script that sends fake data',
+    add_help=True)
+parser.add_argument(
+    '-t', '--time', action='store', type=int,
+    default=float('Inf'), help="""Length of time to send data in seconds
+    (duration). If nothing is selected it will continuously send out data.""")
+parser.add_argument(
+    '-r', '--rate', action='store', type=float, default=1,
+    help="""Rate of data in data sent per second. Default is 1 data pack sent
+    per second.""")
+parser.add_argument(
+    '--host', action='store', type=str, default="localhost",
+    help="""Address of the MQTT broker. If nothing is selected it will
+    default to localhost.""")
+parser.add_argument(
+    '-i', '--id', action='store', type=int, default=None,
+    help="""Specify the sensor to produce fake data. eg. --id 1 specifies that
+    sensor 1 only produces data. If nothing is given all sensors will be
+    active.""")
+
+# Generate the fake sensors with average values
+s_steering_angle = MockSensor(10)
+s_co2 = MockSensor(325)
+s_temperature = MockSensor(25)
+s_humidity = MockSensor(85)
+s_reed_velocity = MockSensor(50)
+s_battery = MockSensor(80)
+s_accelerometer = MockSensor(("x", 90),
+                             ("y", 90),
+                             ("z", 90))
+s_gyroscope = MockSensor(("x", 90),
+                         ("y", 90),
+                         ("z", 90))
+s_gps = MockSensor(("speed", 50),
+                   ("satellites", 10),
+                   ("latitude", 25),
+                   ("longitude", 25),
+                   ("altitude", 50),
+                   ("course", 0))
+
+
+def send_fake_data(client, duration, rate, sensor_id):
+    """ Send artificial data over MQTT for each sensor chanel. Sends [rate] per
+    second for [duration] seconds"""
+
+    start_time = round(time.time(), 2)
+    total_time = 0
+
+    while total_time < duration:
+        current_time = round(time.time(), 2)
+        total_time = round(current_time - start_time, 2)
+        print()     # Newline for clarity
+
+        # TODO: create function that outputs the wireless data output so that
+        # it can be compaired with the the data read by the wireless logging
+        # script
+
+        # Wireless Sensor 1 (Middle)
+        if sensor_id == 1 or sensor_id is None:
+            sensor_data = {
+                            "sensors": [
+                                {
+                                    "type": "temperature",
+                                    "value": s_temperature.get_value()
+                                },
+                                {
+                                    "type": "humidity",
+                                    "value": s_humidity.get_value()
+                                },
+                                {
+                                    "type": "steeringAngle",
+                                    "value": s_steering_angle.get_value()
+                                }
+                             ]
+                          }
+            battery_data = {"percentage": s_battery.get_value()}
+
+            sensor_topic = "v3/wireless-sensor/1/data"
+            battery_topic = "v3/wireless-sensor/1/battery"
+
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
+
+        # Wireless Sensor 2 (Back)
+        if sensor_id == 2 or sensor_id is None:
+            sensor_data = {
+                            "sensors": [
+                                {
+                                    "type": "co2",
+                                    "value": s_co2.get_value()
+                                },
+                                {
+                                    "type": "temperature",
+                                    "value": s_temperature.get_value()
+                                },
+                                {
+                                    "type": "humidity",
+                                    "value": s_humidity.get_value()
+                                },
+                                {
+                                    "type": "accelerometer",
+                                    "value": s_accelerometer.get_value()
+                                },
+                                {
+                                    "type": "gyroscope",
+                                    "value": s_gyroscope.get_value()
+                                }
+                             ]
+                          }
+            battery_data = {"percentage": s_battery.get_value()}
+
+            sensor_topic = "v3/wireless-sensor/2/data"
+            battery_topic = "v3/wireless-sensor/2/battery"
+
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
+
+        # Wireless Sensor 3 (Front)
+        if sensor_id == 3 or sensor_id is None:
+            sensor_data = {
+                            "sensors": [
+                                {
+                                    "type": "co2",
+                                    "value": s_co2.get_value()
+                                },
+                                {
+                                    "type": "reedVelocity",
+                                    "value": s_reed_velocity.get_value()
+                                },
+                                {
+                                    "type": "gps",
+                                    "value": s_gps.get_value()
+                                }
+                             ]
+                          }
+            battery_data = {"percentage": s_battery.get_value()}
+
+            sensor_topic = "v3/wireless-sensor/3/data"
+            battery_topic = "v3/wireless-sensor/3/battery"
+
+            publish(client, sensor_topic, sensor_data)
+            publish(client, battery_topic, battery_data)
+
+        time.sleep(1/rate)
+
+
+def publish(client, topic, data):
+    """
+    Publishes python dict data to a specific topic in JSON and prints it out
+    client: MQTT client object
+    topic:  MQTT topic eg. '/v3/wireless-module/<id>/start'
+    data:   Python dict containing the data to be published on the topic
+    """
+    # Generate JSON from the python dict
+    json_data = json.dumps(data)
+
+    # Publish the data over MQTT
+    client.publish(topic, json_data)
+    print(topic, "--> ", json_data)
+
+
+def start_publishing(client, args):
+    print("publishing started...")
+    send_fake_data(client, args.time, args.rate, args.id)
+    print("publishing finished")
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected Sucessfully! Result code: " + str(rc))
+    else:
+        print("Something went wrong! Result code: " + str(rc))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    broker_address = args.host
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(broker_address)
+    client.loop_start()
+    start_publishing(client, args)
+    client.loop_stop()
