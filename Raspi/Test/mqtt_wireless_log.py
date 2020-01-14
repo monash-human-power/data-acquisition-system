736703b4ab8b9705ee37b40e3db9f93d8a4f0336
Blake <bhay0010@student.monash.edu>
2020-01-14 21:27:53 +1100

    Add identification of temp files

--- Raspi/Test/mqtt_wireless_log.py
+++ Raspi/Test/mqtt_wireless_log.py
@@ -47,8 +47,70 @@ def on_message(client, userdata, msg):
     # Check to see if the topic ends in "data", selecting only the msg's that
     # have wireless_module_data
     if msg.topic[:19] == "/v3/wireless-module" and msg.topic[-4:] == "data":
-        data = parse_wireless_module_data(msg)
-        temp_csv('sensor1', data)
+        sensor_id = str(msg.topic[20])
+        data_dict = parse_wireless_module_data(msg)
+        temp_csv('S'+sensor_id, data_dict)
+
+
+def merge_temps(output_filename):
+    """Searches throught the current directory and finds all the temp files and merges them
+    it finds a """
+    current_dir = os.path.dirname(__file__)
+    filenames = os.listdir(current_dir)
+
+    temp_filenames = []
+
+    for filename in filenames:
+        # Find the temp files and store in the array
+        if filename[-4:]==".csv" and filename[:6]==".~temp":
+            temp_filenames.append(current_dir + '/' + filename)
+
+    merge_temps_continued(output_filename, temp_filenames)
+
+def merge_temps_continued(output_filename, temp_filenames):
+    end_of_csv = False
+    current_line_num = 0
+    while not end_of_csv:
+
+        merged_csv_fieldnames = []
+        current_merged_dict = {}
+        current_dir = os.path.dirname(__file__)
+        merged_csv_path = os.path.join(current_dir, str(output_filename + '.csv'))
+
+        for filename in temp_filenames:
+            csv_file = open(filename, mode='r')
+            csv_reader = csv.reader(csv_file, delimiter=',')
+
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+
+                    print(row)
+                else:
+                    print('0')
+
+                line_count += 1
+                # for_row_num
+                #
+                # current_row_num
+                # # First line
+                # if line_num == 0:
+                #
+                #
+                #     print('HEADERS -->', )
+                # else:
+                #     for i in range(line_num):
+                #         if i == line_num:
+                #             print()
+
+        end_of_csv = True
+
+
+        #write to merged csv
+        with open(merged_csv_path, mode='a') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=merged_csv_fieldnames)
+            csv_writer.writeheader()
+            csv_writer.writerow(current_merged_dict)
 
 
 def parse_wireless_module_data(msg):
@@ -62,7 +124,8 @@ def parse_wireless_module_data(msg):
         sensor_type = sensor["type"]
         sensor_value = sensor["value"]
         if isinstance(sensor_value, dict):
-            print(sensor_value)
+            # print(sensor_value)
+            pass
         else:
             data_dict[sensor_type] = sensor_value
 
@@ -77,11 +140,10 @@ def temp_csv(data_name, data_dict):
     current_dir = os.path.dirname(__file__)
     temp_file_path = os.path.join(current_dir, str('.~temp_' + data_name + '.csv'))
 
-    print(type(data_dict.keys()))
     fieldnames = []
     for key in data_dict.keys():
         fieldnames.append(key)
-    print(temp_file_path)
+    # print(temp_file_path)
 
     if not os.path.exists(temp_file_path):
         # If the temp file does not exist write the headers for the CSV
@@ -96,15 +158,13 @@ def temp_csv(data_name, data_dict):
 
 
 if __name__ == "__main__":
-    # GO WITH 3 CSVs
-    # TODO: REMOVE THIS FOR CLI VERSION
-
-    broker_address = 'localhost'
-    client = mqtt.Client()
-
-    client.on_connect = on_connect
-    client.on_message = on_message
-
-    client.connect(broker_address)
-
-    client.loop_forever()
+    merge_temps('merged_output')
+    # broker_address = 'localhost'
+    # client = mqtt.Client()
+    #
+    # client.on_connect = on_connect
+    # client.on_message = on_message
+    #
+    # client.connect(broker_address)
+    #
+    # client.loop_forever()
