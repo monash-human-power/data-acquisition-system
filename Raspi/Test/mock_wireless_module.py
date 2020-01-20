2a626e65cfd182ef08d1431e63ef08713198cc00
Blake <bhay0010@student.monash.edu>
2020-01-20 17:53:18 +1100

    Add correct stopping of modules

--- Raspi/Test/mock_wireless_module.py
+++ Raspi/Test/mock_wireless_module.py
@@ -180,24 +180,30 @@ def start_publishing(client, args):
 
     # Initiate the correct modules when starting
     if args.id is None:
-        print('started module 1\nstarted module 2\nstarted module 3')
         for i in range(1, 4):
             publish(client, "/v3/wireless-module/" + str(i) + "/start", {
                 "filename": "M" + str(i)
                 + "_" + str(round(time.time()))
                 + ".csv"
             })
+        print('started module 1\nstarted module 2\nstarted module 3')
+
     else:
-        print('started module ' + str(args.id))
         publish(client, "/v3/wireless-module/" + str(args.id) + "/start", {
             "filename": "M" + str(args.id)
             + "_" + str(round(time.time()))
             + ".csv"
         })
+        print('started module ' + str(args.id))
+
 
     send_fake_data(client, args.time, args.rate, args.id)
 
-    print("publishing finished")
+    for i in range(1, 4):
+        publish(client, "/v3/wireless-module/" + str(i) + "/stop", {})
+    print('stopped module 1\nstopped module 2\nstopped module 3')
+
+    print("\npublishing finished")
 
 
 def on_connect(client, userdata, flags, rc):
