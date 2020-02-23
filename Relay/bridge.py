2ed2fe2de6df35205bf19c45ce4365e57c846653
Angus Trau <me@angus.ws>
2020-02-23 18:01:25 +1100

    Create a very basic frame encoder/decoder

---
+++ Relay/bridge.py
@@ -0,0 +1,50 @@
+import time
+import sys
+import argparse
+import paho.mqtt.client as mqtt
+# from zetarf433 import PyZetaRF433
+
+PACKET_LENGTH = 8
+
+parser = argparse.ArgumentParser(
+  description='MQTT to Zeta433 bridge.',
+  add_help=True
+)
+parser.add_argument(
+  '-c', '--channel', action='store', type=int, default=4,
+  help='Zeta433 channel to communicate over. Defaults to 4.'
+)
+parser.add_argument(
+  '--host', action='store', type=str, default="localhost",
+  help="""Address of the MQTT broker. Defaults to localhost."""
+)
+args = parser.parse_args()
+zeta_channel = args.channel
+broker_address = args.host
+
+def on_mqtt_connect(client, userdata, flags, rc):
+  print('Connected to MQTT broker')
+  mqtt_client.subscribe('#')
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_mqtt_connect
+mqtt_client.connect(broker_address)
+mqtt_client.loop_start()
+
+# zeta = PyZetaRF433(PACKET_LENGTH)
+# print('Starting Zeta TxRx...')
+# if not zeta.begin():
+#   print('Zeta begin failed')
+#   sys.exit()
+
+# if not zeta.start_listening_on_channel(zeta_channel):
+#   print('Failed to begin listening')
+#   sys.exit()
+
+def on_mqtt_message(client, userdata, msg):
+  print(msg.topic)
+mqtt_client.on_message = on_mqtt_message
+
+while True:
+  # time.sleep(0.01)
+  pass
