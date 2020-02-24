05b1374120f2b541e473484f306c4297574327fa
Angus Trau <me@angus.ws>
2020-02-24 15:49:04 +1100

    Create basic MQTT-Zeta bridging loginc

--- Relay/bridge.py
+++ Relay/bridge.py
@@ -1,10 +1,10 @@
 import time
 import sys
 import argparse
+import json
 import paho.mqtt.client as mqtt
-# from zetarf433 import PyZetaRF433
-
-PACKET_LENGTH = 8
+from zetarf433 import PyZetaRF433
+from .protocol import RXProtocol, TXProtocol, PACKET_LENGTH
 
 parser = argparse.ArgumentParser(
   description='MQTT to Zeta433 bridge.',
@@ -22,6 +22,9 @@ args = parser.parse_args()
 zeta_channel = args.channel
 broker_address = args.host
 
+rx = RXProtocol()
+tx = TXProtocol()
+
 def on_mqtt_connect(client, userdata, flags, rc):
   print('Connected to MQTT broker')
   mqtt_client.subscribe('#')
@@ -31,20 +34,32 @@ mqtt_client.on_connect = on_mqtt_connect
 mqtt_client.connect(broker_address)
 mqtt_client.loop_start()
 
-# zeta = PyZetaRF433(PACKET_LENGTH)
-# print('Starting Zeta TxRx...')
-# if not zeta.begin():
-#   print('Zeta begin failed')
-#   sys.exit()
+zeta = PyZetaRF433(PACKET_LENGTH)
+print('Starting Zeta TxRx...')
+if not zeta.begin():
+  print('Zeta begin failed')
+  sys.exit()
 
-# if not zeta.start_listening_on_channel(zeta_channel):
-#   print('Failed to begin listening')
-#   sys.exit()
+if not zeta.start_listening_on_channel(zeta_channel):
+  print('Failed to begin listening')
+  sys.exit()
+
+send_queue = []
+
+def on_zeta_message(_rx, message):
+  (topic, payload) = json.loads(message)
+  print(f'RX {topic}')
+  mqtt_client.publish(topic, payload)
+rx.on_message = on_zeta_message
 
 def on_mqtt_message(client, userdata, msg):
-  print(msg.topic)
+  print(f'TX {msg.topic}')
+  payload = json.dumps([msg.topic, msg.payload])
+  send_queue += tx.pack(payload)
 mqtt_client.on_message = on_mqtt_message
 
 while True:
-  # time.sleep(0.01)
-  pass
+  if zeta.check_received():
+    packet = zeta.read_packet(PACKET_LENGTH)
+    rx.receive_packet(packet)
+  time.sleep(0.01)
