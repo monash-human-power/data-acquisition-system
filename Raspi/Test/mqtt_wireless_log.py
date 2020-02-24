188684818e124a70ddd990ae23f01097c017be40
Blake <bhay0010@student.monash.edu>
2020-02-25 10:08:49 +1100

    Wireless Module Logger (#36)
    * Add mqtt_wireless_log.py
    
    * Import necessary libraries
    
    * Add steps needed to complete
    
    * Add ability to connect via MQTT
    
    * Add MQTT subscription to battery and data topics
    
    * Move subscriptions to MQTT on_connect callback function
    
    * Add funct to parse wireless module data into py dict
    
    * Add better ckeck for module data
    
    * Add correct data splitting for wireless module data
    
    * Experiment data splitting and merging with Pandas
    
    * Add temp CSV creation
    
    * Remove pandas code as it will not be used
    
    * Remove redundant csv code
    
    * Add MQTT to CSV logging to individual sensors
    
    * Add identification of temp files
    
    * CSV combiner working
    
    * Remove unnecessary prints and comments
    
    * Correct temp csv columns
    
    * Convert functions to class to aid readability
    
    * Remove redundant data to CSV code
    
    * Add correct chack for wireless sensor in class
    
    * Add subsensor logging
    
    * Add parsing for battery MQTT topics
    
    * Add all parsing code for low battery
    
    * Add recording flags in dict
    
    * Refactor Data to CSV class for readability
    
    * Add searching for temp files
    
    * Replace indexing with str.endswith
    
    * Fix deletion of temp files functionality
    
    * Fix documentation
    
    * Fix spelling mistakes
    
    * Fix MQTT topics and cleaned
    
    * Fix comments and variable names
    
    * Clean MQTT on_connect function
    
    * Clean subscription code for all module channels
    
    * Refactor start and stop recording
    
    * Refactor on_connect to remove unnecessary else statement
    
    * Fix MQTT low battery topic
    
    * Add documentation for save_temp_csv
    
    * Add documentation for merge_temps
    
    * Add main documentation
    
    * Fix delta time recording and low bat
    
    * Fix sensor name varible
    
    * Add documentation for temp csv creation
    
    * Use string split instead of absolute indexing for module_id
    
    * Move global variables to top
    
    * Move start, stop and low bat into thier own functions
    
    * Remove 'is True'
    
    * Add argparse for defining MQTT broker host address
    
    * Remove redundant keys to list
    
    * Remove redundand string conversion
    
    * More readable sub sensor value generation
    
    * Add documentation
    
    * Refactor init
    
    * Move helper functions to top
    
    * Remove 'self' from functions
    
    * Fix make temp issue, fully converted from class to function

---
+++ Raspi/Test/mqtt_wireless_log.py
@@ -0,0 +1,176 @@
+import paho.mqtt.client as mqtt
+import os
+import pandas as pd
+from DataToTempCSV import DataToTempCSV
+import json
+from datetime import datetime
+import argparse
+
+# Global dicts to store state
+# Dict structure is {<module_id> : <data>}
+is_recording = {}       # If the data is being recorded
+module_start_time = {}  # When the data started being recorded
+output_filename = {}    # Output filename
+
+
+parser = argparse.ArgumentParser(
+    description='MQTT wireless logger',
+    add_help=True)
+parser.add_argument(
+    '--host', action='store', type=str, default="localhost",
+    help="""Address of the MQTT broker. If nothing is selected it will
+    default to localhost.""")
+
+
+def on_connect(client, userdata, flags, rc):
+    """ When the MQTT client connects to the broker it prints out if it
+    connects successfully and then subscribes to all of the wireless-module
+    topics"""
+
+    if rc == 0:
+        print("Connected Successfully! Result code: " + str(rc))
+    else:
+        print("Something went wrong! Result code: " + str(rc))
+
+    # Subscribe to all of the wireless module topics
+    client.subscribe("/v3/wireless-module/#")
+
+
+def on_message(client, userdata, msg):
+    """ MQTT callback for when data is sent on the subscribed
+    '/v3/wireless-module/#' topics. The module_id is found by squashing M
+    infront of the module number. eg M1, M2, M3... etc. The module_id is used
+    for identifying what data came from where and is also used for naming the
+    temp files.
+    """
+
+    module_id = "M" + msg.topic.split("/")[3]
+
+    # Start the recording of <module_id>
+    if msg.topic.endswith("start"):
+        start_recording(msg, module_id)
+        print(module_id, "STARTED")
+
+    # Stop the recording of <module_id>
+    elif msg.topic.endswith("stop"):
+        stop_recording(module_id)
+        print(module_id, "STOPPED")
+
+    # Record low battery data
+    elif msg.topic == "/v3/wireless-module/battery/low":
+        record_low_battery(msg)
+
+    # Record other data (battery and sensor data)
+    elif is_recording[module_id]:
+        DataToTempCSV(msg, module_start_time[module_id], module_id)
+
+
+def start_recording(msg, module_id):
+    """ Start recording for a specific module """
+    # Clear the old temporary files in the folder, just in case the script
+    # exited early and they were not cleared properly
+    remove_files(find_temp_csvs(module_id))
+
+    start_data = msg.payload.decode("utf-8")
+    start_data = json.loads(start_data)
+
+    # Save the state of recording and the output filename to global dicts
+    is_recording[module_id] = True
+    module_start_time[module_id] = datetime.now()
+    output_filename[module_id] = start_data["filename"]
+
+
+def stop_recording(module_id):
+    """ Stop recording for a specific module """
+    # Change the state of recording to false in global dict
+    is_recording[module_id] = False
+
+    # Save the temp CSV data into a proper CSV
+    save_temp_csv(module_id)
+
+
+def record_low_battery(msg):
+    """ Record the low battery data for on message """
+    module_data = msg.payload.decode("utf-8")
+    module_data = json.loads(module_data)
+    module_id = "M" + module_data["module-id"]
+
+    DataToTempCSV(msg, module_start_time[module_id], module_id)
+
+
+def save_temp_csv(module_id):
+    """ Saves the temp csvs into real csvs where the battery data and the
+    module data is combined into a single CSV"""
+
+    # Find the temp files in the current folder for the current module
+    temp_filepaths = find_temp_csvs(module_id)
+
+    # Merge the battery and sensor data into a single CSV
+    merge_temps(output_filename[module_id], temp_filepaths)
+
+    # Remove the temp files for the specific module that where generated
+    remove_files(temp_filepaths)
+
+
+def find_temp_csvs(module_id=""):
+    """ Searches throught the current directory and finds all the temp files
+    for a specific module_id and returns the filepaths in a list. If no
+    module_id is given then all temporary files will be found and returned
+    in a list."""
+
+    current_dir = os.path.dirname(__file__)
+
+    # Depends on where the python script is called from
+    if current_dir == '':
+        filepaths = os.listdir()
+    else:
+        filepaths = os.listdir(current_dir)
+
+    # Find temp filepaths and store in the temp_filepaths list
+    temp_filepaths = []
+    for filename in filepaths:
+        if filename.startswith(".~temp_" + module_id) and filename.endswith(".csv"):
+            if current_dir == '':
+                temp_filepaths.append(filename)
+            else:
+                temp_filepaths.append(current_dir + '/' + filename)
+
+    return temp_filepaths
+
+
+def remove_files(filepaths):
+    """ Removes the files in a list of filepaths
+    filepaths: Example list of filepaths is [filepath1, filepath2, filepath3]
+    """
+    for file in filepaths:
+        os.remove(file)
+
+
+def merge_temps(output_filename, temp_filepaths):
+    """ This function merges multiple temporary module CSVs into a final one
+    and names the file correctly.
+    output_filename:    The name of the csv that is generated (String)
+    temp_filepaths:     Example list of filepaths is [filepath1, filepath2]"""
+
+    # Place all of the pandas dataframes into a list
+    temp_dataframes = []
+    for temp_filename in temp_filepaths:
+        df = pd.read_csv(temp_filename)
+        temp_dataframes.append(df)
+
+    # Merge the dataframes and output the final csv
+    merged_dataframe = pd.concat(temp_dataframes, axis=1)
+    merged_dataframe.to_csv(output_filename)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    broker_address = args.host
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(broker_address)
+
+    client.loop_forever()
