57a1dccfc9e16fdcfe1980cc15cf42ded7a13163
Nicholas Chua <nmchua8@gmail.com>
2020-03-06 21:50:40 +1100

    Create loop for indexes sorting into seconds

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -1,25 +1,30 @@
 import pandas as pd 
 from math import *
 
-data = pd.read_excel("C:\\Users\JohnN\Documents\TheStuffOnHere\MHP\das_data_sorter\data_173.xlsx",sheet_name=1)
-time = data['time']
-print(time)
+data = pd.read_excel("C:\\Users\JohnN\Documents\TheStuffOnHere\MHP\data_173.xlsx",sheet_name=1)
+time_array = data['time']/1000
 
 # sorts data into seconds
 seconds = []
-for i in range(len(time)):
-    previous_time = 0
-    if time[i]/1000 > previous_time:
-        #pushes current array of indexes into seconds array, updates previous_time variable, and creates a new array of indexes
-        if previous_time != 0:
-            #excludes first time value
-            seconds.append(time_index)
+previous_time = 0
+for i in range(len(time_array)):
+    if time_array[i] > previous_time:
+        # sets new previous_time, pushes current i_array into seconds array, creates new i_array
+        if previous_time !=0:
+            #ignores first iteration
+            seconds.append(i_array)
 
-        previous_time = ceil(time[i]/1000)
-        time_index = []
-        time_index.append(i)
+        previous_time = ceil(time_array[i])
+        i_array = []
+        i_array.append(i)
     else:
-        #adds to current array
-        time_index.append(i)
-
+        # pushes index into current i_array, if not greater than previous_time
+        i_array.append(i)
+seconds.append(i_array)
 print(seconds)
+
+# nested for loop for future reference when iterating through each "second"
+for i in range(len(seconds)):
+    for j in range(len(seconds[i])):
+        print(time_array[seconds[i][j]])
+    print("one second")
