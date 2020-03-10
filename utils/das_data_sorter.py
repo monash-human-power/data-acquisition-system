28905a9cdd63c59433a99acc7b0235ea458ea5ed
Nicholas Chua <nmchua8@gmail.com>
2020-03-10 14:24:14 +1100

    Fix formatting errors and commenting 2

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -25,41 +25,41 @@ seconds = []
 previous_time = 0
 for i in range(len(time)):
     if time[i] > previous_time:
-        # sets new previous_time, pushes current i_array into seconds array, creates new i_array
-        # i_array is an array of indexes for milliseconds within the same second
+        # sets new previous_time, pushes current millisecond_indices into seconds array, creates new millisecond_indices
+        # millisecond_indices is an array of indexes for milliseconds within the same second
         if previous_time != 0:
             #ignores first iteration
-            seconds.append(i_array)
+            seconds.append(millisecond_indices)
 
         previous_time = ceil(time[i])
-        i_array = []
+        millisecond_indices = []
 
-    # pushes index into current i_array
-    i_array.append(i)
-seconds.append(i_array)
+    # pushes index into current millisecond_indices
+    millisecond_indices.append(i)
+seconds.append(millisecond_indices)
 
 # averages data within the seconds, according to seconds array 
-def avgData(array):
+def avg_Data(array):
     avg_array = []
     for second in seconds:
         total = 0
         for i in second:
-            total = total + array[i]
+            total += array[i]
 
         avg = round(total/len(second), ndigits=2)
         avg_array.append(avg)
     return avg_array
 
-#writes newly-manipulated data into new csv file
+# writes newly-manipulated data into new csv file
 final = pd.DataFrame({
     "time": range(1, len(seconds)+1),
-    "gps_course": avgData(gps_course),
-    "gps_speed": avgData(gps_speed),
-    "tempC": avgData(tempC),
-    "tempF": avgData(tempF),
-    "cadence": avgData(cadence),
-    "power": avgData(power),
-    "reed_velocity": avgData(reed_vel),
-    "reed_distance": avgData(reed_dis)
+    "gps_course": avg_Data(gps_course),
+    "gps_speed": avg_Data(gps_speed),
+    "tempC": avg_Data(tempC),
+    "tempF": avg_Data(tempF),
+    "cadence": avg_Data(cadence),
+    "power": avg_Data(power),
+    "reed_velocity": avg_Data(reed_vel),
+    "reed_distance": avg_Data(reed_dis)
 })
 final.to_csv(args.output, index=False)
