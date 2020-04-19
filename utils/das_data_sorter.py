9e70d5f206183b035a23139521beb6b0c95214d2
Nicholas Chua <nmchua8@gmail.com>
2020-04-19 23:48:23 +1000

    Rearrange functions to top of script

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -2,6 +2,19 @@ import pandas as pd
 import argparse 
 from math import ceil
 
+# averages data within the seconds, according to seconds array 
+def avg_Data(array):
+    avg_array = []
+
+    for index_array in new_time:
+        total = 0
+        for index in index_array:
+            total += array[index]
+
+        avg = round(total/len(index_array), ndigits=2)
+        avg_array.append(avg)
+    return avg_array
+
 # accepts terminal commands
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", help="Input CSV file", action="store", required=True)
@@ -47,19 +60,6 @@ for i in range(len(time)):
     millisecond_indices.append(i)
 
 new_time.append(millisecond_indices)
-
-# averages data within the seconds, according to seconds array 
-def avg_Data(array):
-    avg_array = []
-
-    for index_array in new_time:
-        total = 0
-        for index in index_array:
-            total += array[index]
-
-        avg = round(total/len(index_array), ndigits=2)
-        avg_array.append(avg)
-    return avg_array
 
 # writes newly-manipulated data into new csv file
 final = pd.DataFrame({
