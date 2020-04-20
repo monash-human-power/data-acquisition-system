614513c64a44ecd7bb9a8afa44dcdd4027e16a3d
Nicholas Chua <nmchua8@gmail.com>
2020-04-20 15:09:09 +1000

    Implement sorting into minutes into das data sorting script  (#46)
    * Add Sorting Data script
    
    * Update .gitignore file with .xlsx extension
    
    * Create loop for indexes sorting into seconds
    
    * Implement averaging for rider stats
    
    * Import newly-manipulated data onto new CSV files
    
    * Change Reed and Time variable names
    
    * Fix formatting errors and commenting
    
    * Fix formatting errors and commenting 2
    
    * Change 'time' to 'milliseconds'
    
    * Add averaging into minutes
    
    * From past PR, add required parameter in parser
    
    * Add choices parameter in parser, and remove else code block in 'time' if statement
    
    * Fix parser code format to fit in code editor screen
    
    * Add pipfiles
    
    * Change i and t variables to more meaningful
    
    * Rearrange functions to top of script
    
    * Add Pipfiles

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -2,15 +2,31 @@ import pandas as pd
 import argparse 
 from math import ceil
 
+# averages data within the seconds, according to seconds array 
+def avg_Data(array):
+    avg_array = []
+
+    for index_array in new_time:
+        total = 0
+        for index in index_array:
+            total += array[index]
+
+        avg = round(total/len(index_array), ndigits=2)
+        avg_array.append(avg)
+    return avg_array
+
 # accepts terminal commands
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", help="Input CSV file", action="store", required=True)
 parser.add_argument("--output", help="Returns the filtered data", default="filtered_data.csv", action="store")
+parser.add_argument("--unit", help="Specify time units (seconds, s, or minutes, m)", default="seconds", 
+                    choices=["seconds", "s", "minutes", "m"], action="store")
 args = parser.parse_args()
 
 # initialising data
 data = pd.read_csv(args.file)
-time = data["time"]/1000
+
+milliseconds = data["time"]
 gps_course = data["gps_course"]
 gps_speed = data["gps_speed"]
 tempC = data["thermoC"]
@@ -20,39 +36,34 @@ power = data["power"]
 reed_vel = data["reed_velocity"]
 reed_dis = data["reed_distance"]
 
-# sorts time into seconds
-seconds = []
+time = []
+if args.unit == "seconds" or args.unit == "s":
+    time = milliseconds/1000
+elif args.unit == "minutes" or args.unit =="m":
+    time = milliseconds/1000/60
+
+# sorts time into specific time unit
+new_time = []
 previous_time = 0
 for i in range(len(time)):
     if time[i] > previous_time:
         # sets new previous_time, pushes current millisecond_indices into seconds array, creates new millisecond_indices
-        # millisecond_indices is an array of indexes for milliseconds within the same second
+        # millisecond_indices is an array of indexes for milliseconds within the same specified unit
         if previous_time != 0:
             # ignores first iteration
-            seconds.append(millisecond_indices)
+            new_time.append(millisecond_indices)
 
         previous_time = ceil(time[i])
         millisecond_indices = []
 
     # pushes index into current millisecond_indices
     millisecond_indices.append(i)
-seconds.append(millisecond_indices)
 
-# averages data within the seconds, according to seconds array 
-def avg_Data(array):
-    avg_array = []
-    for second in seconds:
-        total = 0
-        for i in second:
-            total += array[i]
-
-        avg = round(total/len(second), ndigits=2)
-        avg_array.append(avg)
-    return avg_array
+new_time.append(millisecond_indices)
 
 # writes newly-manipulated data into new csv file
 final = pd.DataFrame({
-    "time": range(1, len(seconds)+1),
+    "time": range(1, len(new_time)+1),
     "gps_course": avg_Data(gps_course),
     "gps_speed": avg_Data(gps_speed),
     "tempC": avg_Data(tempC),
