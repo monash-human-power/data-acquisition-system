90424cc2d91769b33a15e921dec0875c03ea9d82
Nicholas Chua <nmchua8@gmail.com>
2020-05-16 19:17:05 +1000

    Make changes on Python file

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -1,62 +1,59 @@
 import pandas as pd 
 from argparse import ArgumentParser
 from numpy import ceil
-from typing import TypeVar
-
-D = TypeVar('DAS_data') # data type to represent columns of the data in a given file
 
 # accepts terminal arguments
 parser = ArgumentParser()
-parser.add_argument("--file", help="Input CSV file", action="store", required=True)
+parser.add_argument("--input", help="Input CSV file", action="store", required=True)
 parser.add_argument("--output", help="Returns the filtered data", default="filtered_data.csv", action="store")
 parser.add_argument("--unit", help="Specify time units (seconds, s, or minutes, m)", default="seconds", 
                     choices=["seconds", "s", "minutes", "m"], action="store")
 args = parser.parse_args()
 
 class DasSort:
-    def __init__(self, data:str) -> None:
+    def __init__(self, file_input:pd.DataFrame, file_output:str, unit:str) -> None:
         self.indexes = None
-        self.time = self.convert_time(data["time"])
-        self.gps = self.average_data(data["gps"])
-        self.gps_location = self.location_data(data["gps_location"])
-        self.gps_course = self.average_data(data["gps_course"])
-        self.gps_speed = self.average_data(data["gps_speed"])
-        self.gps_satellites = self.average_data(data["gps_satellites"])
-        self.ax = self.average_data(data["aX"])
-        self.ay = self.average_data(data["aY"])
-        self.az = self.average_data(data["aZ"])
-        self.gx = self.average_data(data["gX"])
-        self.gy = self.average_data(data["gY"])
-        self.gz = self.average_data(data["gZ"])
-        self.thermoc = self.average_data(data["thermoC"])
-        self.thermof = self.average_data(data["thermoF"])
-        self.pot = self.average_data(data["pot"])
-        self.cadence = self.average_data(data["cadence"])
-        self.power = self.average_data(data["power"])
-        self.reed_velocity = self.average_data(data["reed_velocity"])
-        self.reed_distance = self.average_data(data["reed_distance"])
+        self.time = self.convert_time(file_input["time"], unit)
+        self.gps = self.average_data(file_input["gps"])
+        self.gps_location = self.location_data(file_input["gps_location"])
+        self.gps_course = self.average_data(file_input["gps_course"])
+        self.gps_speed = self.average_data(file_input["gps_speed"])
+        self.gps_satellites = self.average_data(file_input["gps_satellites"])
+        self.ax = self.average_data(file_input["aX"])
+        self.ay = self.average_data(file_input["aY"])
+        self.az = self.average_data(file_input["aZ"])
+        self.gx = self.average_data(file_input["gX"])
+        self.gy = self.average_data(file_input["gY"])
+        self.gz = self.average_data(file_input["gZ"])
+        self.thermoc = self.average_data(file_input["thermoC"])
+        self.thermof = self.average_data(file_input["thermoF"])
+        self.pot = self.average_data(file_input["pot"])
+        self.cadence = self.average_data(file_input["cadence"])
+        self.power = self.average_data(file_input["power"])
+        self.reed_velocity = self.average_data(file_input["reed_velocity"])
+        self.reed_distance = self.average_data(file_input["reed_distance"])
 
-        self.print_output_file()
+        self.write_to_output_file(file_output)
 
-    def convert_time(self, milliseconds:D) -> D:
+    def convert_time(self, milliseconds:pd.Series, unit) -> pd.Series:
         '''Returns the conversion of the time data points from milliseconds to the specified time unit, depending on the 
-        argument passed by args.unit.
+        argument passed by unit.
         
-        args.unit accepts only seconds/s and minutes/m as arguments. 
+        unit accepts only seconds/s and minutes/m as arguments. 
         '''
-        if args.unit == "seconds" or args.unit == "s":
-            milliseconds = milliseconds/1000
-        elif args.unit == "minutes" or args.unit == "m":
-            milliseconds = milliseconds/1000/60
+        if unit == "seconds" or unit == "s":
+            new_time = milliseconds/1000
+        elif unit == "minutes" or unit == "m":
+            new_time = milliseconds/1000/60
         else:
             raise ValueError("Argument only accepts either seconds/s or minutes/m")
         
         # sets the groups of indexes to use for averaging in future
-        self.indexes = self.group_index(milliseconds)
+        self.indexes = self.group_index(new_time)
 
         return range(1,len(self.indexes)+1)
 
-    def group_index(self, time:D) -> D:
+    def group_index(self, time:pd.Series) -> pd.Series:
         '''Returns a universal array of arrays of indexes, based on the specified time unit. Each array represents the indexes 
         of the data points within the same time interval. (eg. 1123ms and 1748ms are in the same time interval for seconds, 
         but 2453ms isn't)
@@ -85,7 +82,7 @@ class DasSort:
 
         return universal_array
 
-    def mean(self, data_array:list) -> int:
+    def mean(self, data_array:pd.Series) -> float:
         '''Finds the average of a given set of numbers. 
 
         Disregards None and zero in calculation. Otherwise, if input is invalid, then raises error.
@@ -93,13 +90,13 @@ class DasSort:
         total = 0
         length = len(data_array)
         for number in data_array:
-            if number == 0 or pd.isna(number):
+            if number > 0 or number < 0:
+                # 'number' value is valid
+                total += number
+            elif number == 0 or pd.isna(number):
                 # None and zeroes are ignored
                 length -= 1
                 continue
-            elif number > 0 or number < 0:
-                # 'number' value is valid
-                total += number
             else:
                 # 'number' value is invalid
                 raise ValueError("Data point invalid and is neither zero nor None. The data point was "+str(number)+". ("+str(type(number))+")")
@@ -110,36 +107,48 @@ class DasSort:
             # in the event where length = 0, due to all the elements in data_array being ignored
             return 0 
 
-    def average_data(self, data:D) -> D:
+    def average_data(self, data:pd.Series) -> pd.Series:
         '''Returns a column of new data points that has been averaged, based on specified time unit. 
 
         Finds the average of all data points within the same time interval. Appends this new value into an array, then continues
         to the next time interval. Once all time intervals have been accounted for, returns the set of new data points.
         '''
-        avg_data = []
+        average_data_array = []
 
         for index_array in self.indexes:
-            avg = self.mean(data[index_array])
-            avg = round(avg, ndigits=2)
-            avg_data.append(avg)
+            current_average = self.mean(data[index_array])
+            current_average = round(current_average, ndigits=2)
+            average_data_array.append(current_average)
 
-        return avg_data
+        return average_data_array
+    
+    # def gps_data(self, data:pd.Series) -> pd.Series:
+        '''currently doesn't work as intended'''
+        
+    #     new_gps_data = []
+    #     for index_array in self.indexes:
+    #         if 1 is in data[index_array]:
+    #             new_gps_data.append(1)
+    #         else:
+    #             new_gps_data.append(0)
+        
+    #     return new_gps_data
 
-    def location_data(self, data:D) -> D:
-        '''Handles the location data and returns the data point at the first element of each time interval. 
+    def location_data(self, data:pd.Series) -> pd.Series:
+        '''Handles the certain gps data and returns the data point at the last element of each time interval. 
 
-        gps_location requires a separate function, because the values are strings, whereas the other data columns are 
-        integers/floats. Handles it so that it will always return the location data at the first element of each time interval.
+        gps_location requires a separate function because the values are strings, whereas the other data columns are 
+        integers/floats. Handles it so that it will always return the location data at the last element of each time interval.
         '''
         new_location_data = []
         for index_array in self.indexes:
-            first_element = index_array[0] 
-            new_location = data[first_element]
-            new_location_data.append(new_location)
+            last_element_index = index_array[-1] 
+            last_element = data[last_element_index]
+            new_location_data.append(last_element)
         
         return new_location_data
     
-    def print_output_file(self) -> None:
+    def write_to_output_file(self, file_output:str) -> None:
         '''Creates new CSV file and writes new data onto CSV file.'''
         final = pd.DataFrame({
             "time": self.time,
@@ -162,9 +171,9 @@ class DasSort:
             "reed_velocity": self.reed_velocity,
             "reed_distance": self.reed_distance
         })
-        final.to_csv(args.output, index=False)
-        print("Success! Check in the same folder for the new CSV file.")
+        final.to_csv(file_output, index=False)
+        print("Success! Output is written to {file_output}")
 
 if __name__ == '__main__':
-    data = pd.read_csv(args.file)
-    das_sort = DasSort(data)
+    data = pd.read_csv(args.input)
+    das_sort = DasSort(data, args.output, args.unit)
