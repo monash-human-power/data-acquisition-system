1137089d41fc83f5dae8703b45e82fedc78c87ec
Nicholas Chua <nmchua8@gmail.com>
2020-05-25 20:32:41 +1000

    Rewrite DAS Data Sort script into Object Orientated Class (#50)
    * Add Sorting Data script
    
    * Update .gitignore file with .xlsx extension
    
    * Create loop for indexes sorting into seconds
    
    * Implement averaging for rider stats
    
    * Import newly-manipulated data onto new CSV files
    
    * Change Reed and Time variable names
    
    * Fix formatting errors and commenting
    
    * Fix formatting errors and commenting 2
    
    * Change 'time' to 'milliseconds'
    
    * Add averaging into minutes
    
    * From past PR, add required parameter in parser
    
    * Add choices parameter in parser, and remove else code block in 'time' if statement
    
    * Fix parser code format to fit in code editor screen
    
    * Add pipfiles
    
    * Change i and t variables to more meaningful
    
    * Rearrange functions to top of script
    
    * Add Pipfiles
    
    * Implement mean_smooth and median_smooth functions
    
    * Adapt avg_data to utilise Statistics module
    
    * Integrate existing code with smoothing function
    
    * Update pipfile
    
    * Rewrite script to Object-Orientated Class
    
    * Fix formatting errors for class methods
    
    * Implement mean function
    
    * Change file name
    
    * Delete das_data_sorter2.py
    
    Duplicate file. 
    
    Also I don't know how to delete files and then pushing that change using the terminal.
    
    * Make changes on Python file
    
    * Remove Pipfiles
    
    * Use f-strings to print success statement
    
    * Fix gps_data code
    
    * Move Write Output File method outside constructor
    
    * Replace gps_location into gps_lat, gps_long, gps_alt
    
    * Remove numpy.int64 method as it is no longer needed
    
    * Removed location_data function, replaced with average_data function
    
    * Install poetry
    
    * Fixed missing gps_lat,gps_long,gps_alt columns on output CSV files

--- utils/das_data_sorter.py
+++ utils/das_data_sorter.py
@@ -1,76 +1,170 @@
 import pandas as pd 
-import argparse 
-from math import ceil
+from argparse import ArgumentParser
+from numpy import ceil
 
-# averages data within the seconds, according to seconds array 
-def avg_Data(array):
-    avg_array = []
-
-    for index_array in new_time:
-        total = 0
-        for index in index_array:
-            total += array[index]
-
-        avg = round(total/len(index_array), ndigits=2)
-        avg_array.append(avg)
-    return avg_array
-
-# accepts terminal commands
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", help="Input CSV file", action="store", required=True)
-parser.add_argument("--output", help="Returns the filtered data", default="filtered_data.csv", action="store")
-parser.add_argument("--unit", help="Specify time units (seconds, s, or minutes, m)", default="seconds", 
+# accepts terminal arguments
+parser = ArgumentParser()
+parser.add_argument("--input", help="Reads the inputted CSV file to filter", action="store", required=True)
+parser.add_argument("--output", help="Writes the filtered data onto a new CSV file under this name", action="store", required=True)
+parser.add_argument("--unit", help="Specify time units [seconds, s, or minutes, m]. Default is in seconds.", default="seconds", 
                     choices=["seconds", "s", "minutes", "m"], action="store")
 args = parser.parse_args()
 
-# initialising data
-data = pd.read_csv(args.file)
+class DasSort:
+    def __init__(self, file_input:pd.DataFrame, unit:str) -> None:
+        self.indexes = None
+        self.time = self.convert_time(file_input["time"], unit)
+        self.gps = self.gps_data(file_input["gps"])
+        self.gps_lat = self.average_data(file_input["gps_lat"])
+        self.gps_long = self.average_data(file_input["gps_long"])
+        self.gps_alt = self.average_data(file_input["gps_long"])
+        self.gps_course = self.average_data(file_input["gps_course"])
+        self.gps_speed = self.average_data(file_input["gps_speed"])
+        self.gps_satellites = self.average_data(file_input["gps_satellites"])
+        self.ax = self.average_data(file_input["aX"])
+        self.ay = self.average_data(file_input["aY"])
+        self.az = self.average_data(file_input["aZ"])
+        self.gx = self.average_data(file_input["gX"])
+        self.gy = self.average_data(file_input["gY"])
+        self.gz = self.average_data(file_input["gZ"])
+        self.thermoc = self.average_data(file_input["thermoC"])
+        self.thermof = self.average_data(file_input["thermoF"])
+        self.pot = self.average_data(file_input["pot"])
+        self.cadence = self.average_data(file_input["cadence"])
+        self.power = self.average_data(file_input["power"])
+        self.reed_velocity = self.average_data(file_input["reed_velocity"])
+        self.reed_distance = self.average_data(file_input["reed_distance"])
 
-milliseconds = data["time"]
-gps_course = data["gps_course"]
-gps_speed = data["gps_speed"]
-tempC = data["thermoC"]
-tempF = data["thermoF"]
-cadence = data["cadence"]
-power = data["power"]
-reed_vel = data["reed_velocity"]
-reed_dis = data["reed_distance"]
+    def convert_time(self, milliseconds:pd.Series, unit) -> pd.Series:
+        '''Returns the conversion of the time data points from milliseconds to the specified time unit, depending on the 
+        argument passed by unit.
+        
+        unit accepts only seconds/s and minutes/m as arguments. 
+        '''
+        if unit == "seconds" or unit == "s":
+            new_time = milliseconds/1000
+        elif unit == "minutes" or unit == "m":
+            new_time = milliseconds/1000/60
+        else:
+            raise ValueError("Argument only accepts either seconds/s or minutes/m")
+        
+        # sets the groups of indexes to use for averaging in future
+        self.indexes = self.group_index(new_time)
 
-time = []
-if args.unit == "seconds" or args.unit == "s":
-    time = milliseconds/1000
-elif args.unit == "minutes" or args.unit =="m":
-    time = milliseconds/1000/60
+        return range(1,len(self.indexes)+1)
 
-# sorts time into specific time unit
-new_time = []
-previous_time = 0
-for i in range(len(time)):
-    if time[i] > previous_time:
-        # sets new previous_time, pushes current millisecond_indices into seconds array, creates new millisecond_indices
-        # millisecond_indices is an array of indexes for milliseconds within the same specified unit
-        if previous_time != 0:
-            # ignores first iteration
-            new_time.append(millisecond_indices)
+    def group_index(self, time:pd.Series) -> pd.Series:
+        '''Returns a universal array of arrays of indexes, based on the specified time unit. Each array represents the indexes 
+        of the data points within the same time interval. (eg. 1123ms and 1748ms are in the same time interval for seconds, 
+        but 2453ms isn't)
 
-        previous_time = ceil(time[i])
-        millisecond_indices = []
+        When given a valid value for args.unit, function will group the indexes within the same time interval in an array.
+        Once all the indexes of a time interval has been accounted for, the array of indexes will be pushed into a
+        universal array so that each element represents the data points of one singular time interval.
+        '''
+        universal_array = []
+        index_array = []
+        previous_time = 0
 
-    # pushes index into current millisecond_indices
-    millisecond_indices.append(i)
+        for i in range(len(time)):
+            if time[i] > previous_time:
+                # if true, then time at time[i] is the next second/minute
+                if previous_time != 0:
+                    # appends index array into universal array, but ignores the first iteration
+                    universal_array.append(index_array)
 
-new_time.append(millisecond_indices)
+                previous_time = ceil(time[i])
+                index_array = [] # recreates new array if previous time has changed
+            index_array.append(i) 
 
-# writes newly-manipulated data into new csv file
-final = pd.DataFrame({
-    "time": range(1, len(new_time)+1),
-    "gps_course": avg_Data(gps_course),
-    "gps_speed": avg_Data(gps_speed),
-    "tempC": avg_Data(tempC),
-    "tempF": avg_Data(tempF),
-    "cadence": avg_Data(cadence),
-    "power": avg_Data(power),
-    "reed_velocity": avg_Data(reed_vel),
-    "reed_distance": avg_Data(reed_dis)
-})
-final.to_csv(args.output, index=False)
+        # pushes the final array when for loop has completed
+        universal_array.append(index_array)
+
+        return universal_array
+
+    def mean(self, data_array:pd.Series) -> float:
+        '''Finds the average of a given set of numbers. 
+
+        Disregards None and zero in calculation. Otherwise, if input is invalid, then raises error.
+        '''
+        total = 0
+        length = len(data_array)
+        for number in data_array:
+            if number > 0 or number < 0:
+                # 'number' value is valid
+                total += number
+            elif number == 0 or pd.isna(number):
+                # None and zeroes are ignored
+                length -= 1
+                continue
+            else:
+                # 'number' value is invalid
+                raise ValueError("Data point invalid and is neither zero nor None. The data point was "+str(number)+". ("+str(type(number))+")")
+        
+        try:
+            return total/length
+        except ZeroDivisionError:
+            # in the event where length = 0, due to all the elements in data_array being ignored
+            return 0 
+
+    def average_data(self, data:pd.Series) -> pd.Series:
+        '''Returns a column of new data points that has been averaged, based on specified time unit. 
+
+        Finds the average of all data points within the same time interval. Appends this new value into an array, then continues
+        to the next time interval. Once all time intervals have been accounted for, returns the set of new data points.
+        '''
+        average_data_array = []
+
+        for index_array in self.indexes:
+            current_average = self.mean(data[index_array])
+            current_average = round(current_average, ndigits=2)
+            average_data_array.append(current_average)
+
+        return average_data_array
+    
+    def gps_data(self, data:pd.Series) -> pd.Series:
+        '''Returns the data array of the time intervals which the GPS was turned on. 
+        
+        0 for when GPS was turned off, 1 for when turned on.
+        '''
+        new_gps_data = []
+        for index_array in self.indexes:
+            if 1 in data[index_array].values:
+                new_gps_data.append(1)
+            else:
+                new_gps_data.append(0)
+        
+        return new_gps_data
+    
+    def write_to_output_file(self, file_output:str) -> None:
+        '''Creates new CSV file and writes new data onto CSV file.'''
+        final = pd.DataFrame({
+            "time": self.time,
+            "gps": self.gps,
+            "gps_lat": self.gps_lat,
+            "gps_long": self.gps_long,
+            "gps_alt": self.gps_alt,
+            "gps_course": self.gps_course,
+            "gps_speed": self.gps_speed,
+            "gps_satellites": self.gps_satellites,
+            "aX": self.ax,
+            "aY": self.ay,
+            "aZ": self.az,
+            "gX": self.gx,
+            "gY": self.gy,
+            "gZ": self.gz,
+            "thermoC": self.thermoc,
+            "tempF": self.thermof,
+            "pot": self.pot,
+            "cadence": self.cadence,
+            "power": self.power,
+            "reed_velocity": self.reed_velocity,
+            "reed_distance": self.reed_distance
+        })
+        final.to_csv(file_output, index=False)
+        print(f"Success! Output is written to {file_output}")
+
+if __name__ == '__main__':
+    data = pd.read_csv(args.input) # Loads and reads CSV file
+    das_sort = DasSort(data, args.unit) # Filters data in CSV file
+    das_sort.write_to_output_file(args.output) # Write filtered data into new CSV file
