66daba31b82fe4386cb8e3ee2a248c76b9f4bfc3
Blake Haydon <bhay0010@student.monash.edu>
2020-06-01 21:40:44 +1000

    Add docs for conneccting to WiFi

--- wireless_modules/mqtt_test/boot.py
+++ wireless_modules/mqtt_test/boot.py
@@ -1,25 +1,33 @@
-from config import ESSID
-from config import PASSWORD
-from machine import Pin
+import config 
 import esp
+import network
+from machine import Pin
+
 
 def connect(eesid, password):
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
+    """ Small function that continuously tries to connect to WiFi """
+    
+    station_interface = network.WLAN(network.STA_IF)
+    if not station_interface.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(eesid, password)
-        while not sta_if.isconnected():
+        
+        # Activate the WiFi interface
+        station_interface.active(True)
+        
+        # Connect to WiFi using the credentials given
+        station_interface.connect(eesid, password)
+
+        # Loop forever until connected to WiFi    
+        while not station_interface.isconnected():
             pass
 
-    print('network config:', sta_if.ifconfig())
+    print('network config:', station_interface.ifconfig())
 
 # Allows the code to be uploaded with tools such as rshell / ampy
 esp.osdebug(None)
 
 # Connect to WiFi using credentials stored in config.py
-connect(ESSID, PASSWORD)
+connect(config.ESSID, config.PASSWORD)
 
 # Turn on the built in LED when booted and connected to WiFi (blue light)
 builtin_LED = Pin(2, Pin.OUT)
