e43e305c5d3c1c77b8f9d1fa97ab2b0dd0e8994e
Kunj Dave <kdav0006@student.monash.edu>
2020-08-03 16:38:34 +1000

    Added error handling when connecting to Broker

--- wireless_modules/middle_module/MQTT_Client_class.py
+++ wireless_modules/middle_module/MQTT_Client_class.py
@@ -1,14 +1,14 @@
 # Author: Kunj (most is also borrowed from mqtt_test written by Blake)
-# Last modified: 1/08/2020
-# Note: You can remove all the PRINT statements when everything is tested
+# Last modified: 3/08/2020
+# Note: Can remove all the Print statements once all testing is successful
 
-import utime
 # Use micro pip to install packages
+import utime
 import upip
 upip.install("micropython-umqtt.simple2")
 try:
     from umqtt.simple import MQTTClient
-except:
+except FileNotFoundError:
     print("Error in importing library")
 
 
@@ -19,36 +19,34 @@ class Client:
         :param client_id: The unique client id used to initiate an MQTTClient class
         :param mqtt_broker: A string holding domain name or IP address of the broker to connect to, to send and receive
                             data.
-        :param callback_func: The function to be called whenever a message is received.
         """
         self.client = MQTTClient(client_id, mqtt_broker)
-
-        # Only used for print statements
         self.mqtt_broker = mqtt_broker
 
     def connect_and_subscribe(self, topics_to_subscribe, callback_func):
         """
         Connects to the MQTT broker and subscribes to topic in 'topics_to_subscribe'
-        :param topics_to_subscribe: An array of topic to subscribe to.
-                                    Eah element must be a string or byte literal (preferred)
+        :param topics_to_subscribe: An array of topics to subscribe to.
+                                    Eah element must be a string or byte literal (the latter is preferred)
         :param callback_func: The function to be called whenever a message from the subscribed topics is received.
-        :return: None
+        :return: True if a connection to the Broker is successfully established, otherwise False
         """
 
         # Provide the function to run whenever a message is received from the subscribed topics
         self.client.set_callback(callback_func)
-
-        # Connect to MQTT broker
-        self.client.connect()
-        print('Connected to %s MQTT broker' % (self.mqtt_broker))
+        try:
+            # Connect to MQTT broker
+            self.client.connect()
+            print('Connected to %s MQTT broker' % (self.mqtt_broker))
+        except OSError:
+            return False
 
         # Subscribe to each topic
         for topic in topics_to_subscribe:
             self.client.subscribe(topic)
             print('Subscribed to %s topic' % (topic))
 
-        # Check for any incoming messages already waiting
-        self.check_for_message()
+        return True
 
     def _to_bytes_literal(self, data):
         """
@@ -81,13 +79,13 @@ class Client:
             # print("MQTT data sent: %s on %s through %s" % (msg, topic, self.mqtt_broker))
 
         except OSError:
-            print("OSError, check line 79 of main.py")
+            print("OSError, check line 80 of MQTT_Client_class.py")
             # restart_and_reconnect()
 
     def check_for_message(self):
         """
         Checks whether an incoming message from the MQTT broker on the subscribed topics is pending. This check is done
-        within the mqtt_pub and connect_and_subscribe method as well, but it gives a way to check manually for any
+        within the mqtt_pub method as well, but it gives a way to check manually for any
         message (especially given that the MQTT Client is only held by this class).
         :return: None
         """
