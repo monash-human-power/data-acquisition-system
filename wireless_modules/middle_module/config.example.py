5870117ad761404fb3589e9c8c972804b36c6756
Kunj Dave <kdav0006@student.monash.edu>
2020-08-05 14:54:03 +1000

    Add example config.py file

---
+++ wireless_modules/middle_module/config.example.py
@@ -0,0 +1,9 @@
+from machine import Pin
+
+ESSID = "<WIFI NETWORK NAME HERE>"
+PASSWORD = "<WIFI PASSWORD HERE>"
+
+MQTT_BROKER = "<Boker domain address or IP address>"
+
+builtin_LED = Pin(2, Pin.OUT)
+
