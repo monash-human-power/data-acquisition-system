e818ba84f5f285178d84e8befef9ce9a5662cf8e
Christopher Hall <hallgchris@gmail.com>
2020-08-09 21:24:12 +1000

    Add Frame documentation

--- Bridge/src/frame.hpp
+++ Bridge/src/frame.hpp
@@ -5,21 +5,42 @@
 #include <ostream>
 #include <stdint.h>
 
+/** Size of the packet body in bytes. */
 constexpr uint16_t BODY_LENGTH = 75;
+/** Total size of the packet in bytes. */
 constexpr uint16_t PACKET_LENGTH = 80;
 
+/**
+ * Types of packets that may be transmitted and received.
+ * The associated value is the ID of the frame type which is sent as part of
+ * the packet.
+ */
 enum class FrameType : uint8_t
 {
     Message = 0,
 };
 
+/**
+ * The format of a message frame.
+ * Each part of the frame appears in the order it is shown below with no
+ * packaging. Everything uses little endian encoding under ARM and x86.
+ */
 struct __attribute__((packed)) Frame
 {
-    uint8_t frame_counter;
+    /** Total number of frames sent, modulo 255. */
+    uint8_t frame_count;
+    /** Type of the frame. Currently always FrameType::Message. */
     FrameType frame_type;
-    uint8_t part_counter;
+    /** Position of the frame within a message, modulo 255. */
+    uint8_t part_count;
+    /** Length of the message body across all frames. */
     uint16_t body_length;
+    /** A slice of the body content, up to 75 bytes in length, zero padded. */
     uint8_t body[BODY_LENGTH] = {0};
 };
 
+/**
+ * Writes the bytes of a Frame to an ostream. Useful for printing the frame's
+ * contents.
+ */
 std::ostream& operator<<(std::ostream& os, const Frame *frame);
