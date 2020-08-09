ba2a4f3609b1a14d0a780c4d448adb1e219ec493
Christopher Hall <hallgchris@gmail.com>
2020-08-09 21:25:17 +1000

    Add MQTT documentation

--- Bridge/src/mqtt.cpp
+++ Bridge/src/mqtt.cpp
@@ -3,7 +3,7 @@
 
 #include "mqtt.hpp"
 
-MqttCallback::MqttCallback(mqtt::async_client_ptr client, mqtt::connect_options& conn_opts)
+MqttCallback::MqttCallback(mqtt::async_client_ptr client)
     : client_(client) {}
 
 void MqttCallback::set_on_message(std::function<void(mqtt::const_message_ptr)> callback)
@@ -13,7 +13,7 @@ void MqttCallback::set_on_message(std::function<void(mqtt::const_message_ptr)> c
 
 void MqttCallback::on_failure(const mqtt::token& asyncActionToken)
 {
-    std::cout << "Connection failed" << std::endl;
+    std::cout << "MQTT connection failed" << std::endl;
     exit(1);
 }
 
@@ -22,7 +22,7 @@ void MqttCallback::on_success(const mqtt::token& asyncActionToken) {}
 
 void MqttCallback::connected(const std::string& cause)
 {
-    std::cout << "Connection success" << std::endl;
+    std::cout << "MQTT connection succeeded" << std::endl;
     this->client_->subscribe("#", 0);
 }
 
@@ -47,18 +47,18 @@ MqttBridgeClient::MqttBridgeClient()
     conn_opts.set_clean_session(true);
     conn_opts.set_automatic_reconnect(MIN_RECONNECT_INTERVAL, MAX_RECONNECT_INTERVAL);
 
-    this->callback_ = std::make_shared<MqttCallback>(this->client_, conn_opts);
+    this->callback_ = std::make_shared<MqttCallback>(this->client_);
     this->client_->set_callback(*callback_);
 
     try
     {
         // Connect to client
-        std::cout << "Connecting..." << std::endl;
+        std::cout << "Connecting to MQTT broker..." << std::endl;
         this->client_->connect(conn_opts);
     }
     catch (const mqtt::exception& exc)
     {
-        std::cerr << "Error: " << exc.what() << " ["
+        std::cerr << "MQTT Error: " << exc.what() << " ["
             << exc.get_reason_code() << "]" << std::endl;
     }
 }
