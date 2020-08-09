b2d7bfc856fe100edd252f9ac3fc5961dcb477f9
Christopher Hall <hallgchris@gmail.com>
2020-08-09 21:24:32 +1000

    Tidy up main.cpp

--- Bridge/src/main.cpp
+++ Bridge/src/main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 
-
-#include "protocol.hpp"
 #include "mqtt.hpp"
-
-void receive_callback(mqtt::const_message_ptr message)
-{
-    std::cout << "Parsed packet!" << std::endl
-        << "\tQoS:      " << message->get_qos() << std::endl
-        << "\tRetained: " << message->is_retained() << std::endl
-        << "\tTopic:    " << message->get_topic() << std::endl
-        << "\tPayload:  " << message->get_payload() << std::endl;
-}
+#include "protocol.hpp"
 
 int main()
 {
     auto mqttClient = std::make_shared<MqttBridgeClient>();
-
     Protocol protocol(mqttClient);
 
     while (std::cin.get() != 'q')
