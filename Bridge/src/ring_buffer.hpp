d1cec0087a1cbd5b3b417c07ef643ce27637f1d3
Christopher Hall <hallgchris@gmail.com>
2020-08-09 22:41:56 +1000

    Add ring buffer documentation

--- Bridge/src/ring_buffer.hpp
+++ Bridge/src/ring_buffer.hpp
@@ -3,18 +3,44 @@
 #include <mutex>
 #include <array>
 
+/**
+ * A very basic, thread safe implementation of a ring buffer.
+ * Supports only adding items and checking if the item is contained. Items
+ * cannot be explicitly removed, only overwritten.
+ * @tparam T The type to be stored in the ring buffer.
+ * @tparam N The size of the ring buffer, i.e. the maximum number of items to
+ *           be stored.
+ */
 template <class T, size_t N>
 class RingBuffer
 {
+    /**
+     * The mutex used to ensure multiple threads cannot simultaneously access
+     * the ring buffer.
+     */
     mutable std::mutex mutex_;
+    /**
+     * Array in which added elements are stored. */
     std::array<T, N> buffer_;
+    /** Iterator pointing to the next element to be written to. */
     typename std::array<T, N>::iterator head_;
+    /** True if the internal array is at capacity. */
     bool full_ = false;
 
 public:
+    /** Create a new ring buffer. */
     RingBuffer();
 
+    /**
+     * Insert an item into the ring buffer.
+     * @param item The item to be added.
+     */
     void put(const T item);
+    /**
+     * Check if an item is contained within the ring buffer.
+     * @param item The item to be queried.
+     * @return True if the item is contained, else false.
+     */
     bool contains(const T item) const;
 };
 
