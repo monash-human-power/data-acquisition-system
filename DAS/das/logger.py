82f64def026b3c18d8fad5e4d78d00eb01ab89a8
Blake Haydon <bhay0010@student.monash.edu>
2020-08-13 12:58:42 +1000

    Add nice recording

--- DAS/das/logger.py
+++ DAS/das/logger.py
@@ -5,9 +5,9 @@ import os
 
 
 class Logger:
-    """ This class logs MQTT data"""
+    """ This class logs MQTT data """
 
-    def __init__(self, csv_folder_path: str, broker_address: str = "localhost", verbose: bool = True, *topics: str) -> None:
+    def __init__(self, csv_folder_path: str, *topics: str, broker_address: str = "localhost", verbose: bool = True) -> None:
         # The logger object can subscribe to many topics (if none are selected then it will subscrive to all)
         if len(topics) != 0:
             self.TOPICS = topics
@@ -23,10 +23,24 @@ class Logger:
         # Create csv_data folder if none exists
         Path(csv_folder_path).mkdir(parents=True, exist_ok=True)
 
-        # Name the csv log file (APPEND ONLY)
-        # TODO: Make variable
-        filename = "0001_log.csv"
+        # Name the csv log file xxxx_log.csv where xxxx is a number
+        # APPEND ONLY to stop accidentally recording over data
+        previous_log_num = 0
+        for filename in os.listdir(csv_folder_path):
+            try:
+                if int(filename[0:4]) > previous_log_num:
+                    previous_log_num = int(filename[0:4])
+            except ValueError:
+                print(
+                    f"WARNING: {filename} should not be in {csv_folder_path}")
+            except Exception as e:
+                print(f"ERROR: {e}")
+
+        # fstring :0>4 used to ensure that the number will always be 4 long
+        filename = f"{previous_log_num + 1:0>4}_log.csv"
         self._LOG_FILE = open(os.path.join(csv_folder_path, filename), "a")
+        # Add headers for csv
+        self._LOG_FILE.write("time_delta, mqtt_topic, message \n")
 
         # Connect to MQTT broker
         self._client = mqtt.Client()
@@ -40,29 +54,34 @@ class Logger:
 
     def _on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            print("Connected Successfully! Result code: " + str(rc))
+            print("Connection Successful!")
         else:
-            print("Something went wrong! Result code: " + str(rc))
+            print("ERROR: Something went wrong!")
 
         # Subscribe to all of the topics
         try:
             for topic in self.TOPICS:
                 self._client.subscribe(topic)
+                if self._VERBOSE:
+                    print(f"Subscribed to: {topic}")
         except Exception as e:
-            print(e)
+            print(f"ERROR: {e}")
 
     def _on_message(self, client, userdata, msg):
         # Log the incoming MQTT message
         try:
             self.log(msg.topic, msg.payload)
         except Exception as e:
-            print(e)
+            print(f"ERROR: {e}")
 
     def log(self, mqtt_topic: str, message: str) -> None:
         time_delta = datetime.now() - self._DATETIME_START
 
-        if self._VERBOSE:
-            print(f"{time_delta} | {mqtt_topic} | {message}")
-
         # Write data to csv file
-        self._LOG_FILE.write(f"{time_delta}, {mqtt_topic}, {message} \n")
+        try:
+            self._LOG_FILE.write(f"{time_delta}, {mqtt_topic}, {message} \n")
+            if self._VERBOSE:
+                # TODO: don't hardcode topic length when printing
+                print(f"{time_delta} | {mqtt_topic: <50} | {message}")
+        except Exception as e:
+            print(f"ERROR: {e}")
--- DAS/das/mqtt_logger_V2.py
+++ DAS/das/mqtt_logger_V2.py
@@ -3,9 +3,6 @@
 # TODO: Do propper error checking
 # TODO: Check that it exits nicely
 
-import paho.mqtt.client as mqtt
-from datetime import datetime
-from pathlib import Path
 import argparse
 import os
 from logger import Logger
@@ -19,50 +16,8 @@ parser.add_argument(
     help="""Address of the MQTT broker. If nothing is selected it will
     default to localhost.""")
 
-
-def on_connect(client, userdata, flags, rc):
-    # TODO: add actual error handling
-    if rc == 0:
-        print("Connected Successfully! Result code: " + str(rc))
-    else:
-        print("Something went wrong! Result code: " + str(rc))
-
-    # Subscribe to all of the topics
-    try:
-        client.subscribe("#")
-    except Exception as e:
-        print(e)
-
-
-def on_message(client, userdata, msg):
-    # Log the incoming MQTT message
-    try:
-        log(msg.topic, msg.payload)
-    except Exception as e:
-        print(e)
-
-
-def log(mqtt_topic, message) -> None:
-    time_delta = datetime.now() - DATETIME_START
-    print(f"{time_delta} | {mqtt_topic} | {message}")
-
-    # Write data to csv file
-    LOG_FILE.write(f"{time_delta}, {mqtt_topic}, {message} \n")
-
-
 if __name__ == "__main__":
-    print("hello")
     CURRENT_FILEPATH = os.path.dirname(__file__)
     CSV_FILEPATH = os.path.join(CURRENT_FILEPATH, "csv_data")
 
-    Logger(CSV_FILEPATH)
-    # args = parser.parse_args()
-    # broker_address = args.host
-    # client = mqtt.Client()
-
-    # client.on_connect = on_connect
-    # client.on_message = on_message
-
-    # client.connect(broker_address)
-
-    # client.loop_forever()
+    Logger(CSV_FILEPATH, "test/#", "tes/#")
