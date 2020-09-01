5df25fba672f741c3d6458a7131300eb385d12a1
Kunj Dave <kdav0006@student.monash.edu>
2020-09-01 18:05:24 +1000

    Add new files

--- wireless_modules/middle_module/upload.sh
+++ wireless_modules/middle_module/upload.sh
@@ -7,3 +7,15 @@ ampy -p $1 -b 115200 put ../boot.py
 echo "boot.py loaded"
 ampy -p $1 -b 115200 put ../mqtt_client.py
 echo "mqtt_client.py loaded"
+ampy -p $1 -b 115200 put ../wireless_module.py
+echo "wireless_module.py loaded"
+ampy -p $1 -b 115200 put mpu6050.py
+echo "mpu6050.py loaded"
+ampy -p $1 -b 115200 put mpu.py
+echo "mpu.py loaded"
+ampy -p $1 -b 115200 put ../common_sensors/dht_sensor.py
+echo "dht_sensor.py loaded"
+ampy -p $1 -b 115200 put ../common_sensors/mq135_sensor.py
+echo "mq135_sensor.py loaded"
+ampy -p $1 -b 115200 put ../MQ135/mq135.py
+echo "mq135.py loaded"
