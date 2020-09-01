db6d51c1b2fe56e63547de529c3eade562547526
Kunj Dave <kdav0006@student.monash.edu>
2020-09-01 18:10:59 +1000

    Add code to ensure dht22 is not read too frequently

--- wireless_modules/common_sensors/dht_sensor.py
+++ wireless_modules/common_sensors/dht_sensor.py
@@ -1,27 +1,29 @@
-# Class for the DHT22 sensor
 import dht
 
 
 class DhtSensor:
-    def __init__(self, pin):
+    def __init__(self, pin, queries=2):
         """
         Initialise the DHT sensor
         :param pin: A Pin object
+        :param queries: Number of times the sensor will be read every 2 seconds
         """
         self.sensor = dht.DHT22(pin)
-
-        # Variable to ensure data is read only once per second
-        self.read_already = False
         self.readings = []
+
+        self.max_queries = queries
+        self.queries_made = 0
 
     def read(self):
         """
         Reads the temperature and humidity measurements from the sensor and returns it as an array of dictionaries
-        :return: An array of length 2 containing dictionary for teh temperature and humidity data
+        :return: An array of length 2 containing dictionary for the temperature and humidity values
         """
-        if self.read_already:
-            self.read_already = False
-        else:
+        if self.queries_made == self.max_queries:
+            self.queries_made = 0
+
+        # read (fresh) data if this if the first query in the 2 second frame, otherwise return previously read data
+        if self.queries_made == 0:
             self.sensor.measure()
             self.readings = [{
                                 "type": "temperature",
@@ -30,11 +32,7 @@ class DhtSensor:
                                 "type": "humidity",
                                 "value": self.sensor.humidity()
                             }]
-            self.read_already = True
 
+        self.queries_made += 1
         return self.readings
 
-    def get_data(self):
-        if len(self.readings) == 0:
-            self.read()
-        return self.readings
