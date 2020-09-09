36727a2c5c0450dd76654d73aab3a0f2e21a9f12
Kunj Dave <kdav0006@student.monash.edu>
2020-09-09 19:57:02 +1000

    Reorganise class files

--- wireless_modules/middle_module/upload.sh
+++ wireless_modules/middle_module/upload.sh
@@ -11,11 +11,11 @@ ampy -p $1 -b 115200 put ../wireless_module.py
 echo "wireless_module.py loaded"
 ampy -p $1 -b 115200 put MPU6050-ESP8266-MicroPython/mpu6050.py
 echo "mpu6050.py loaded"
-ampy -p $1 -b 115200 put mpu.py
+ampy -p $1 -b 115200 put ../sensors/mpu.py
 echo "mpu.py loaded"
-ampy -p $1 -b 115200 put ../common_sensors/dht_sensor.py
+ampy -p $1 -b 115200 put ../sensors/dht_sensor.py
 echo "dht_sensor.py loaded"
-ampy -p $1 -b 115200 put ../common_sensors/mq135_sensor.py
+ampy -p $1 -b 115200 put ../sensors/co2_sensor.py
 echo "mq135_sensor.py loaded"
 ampy -p $1 -b 115200 put ../MQ135/mq135.py
 echo "mq135.py loaded"
