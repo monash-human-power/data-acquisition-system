757bffe99223d165032af68cdcfc27c44d2bf01d
Kunj Dave <kdav0006@student.monash.edu>
2020-09-09 20:18:14 +1000

    Update upload.sh with files changes

--- wireless_modules/middle_module/upload.sh
+++ wireless_modules/middle_module/upload.sh
@@ -9,7 +9,7 @@ ampy -p $1 -b 115200 put ../mqtt_client.py
 echo "mqtt_client.py loaded"
 ampy -p $1 -b 115200 put ../wireless_module.py
 echo "wireless_module.py loaded"
-ampy -p $1 -b 115200 put MPU6050-ESP8266-MicroPython/mpu6050.py
+ampy -p $1 -b 115200 put ../libraries/MPU6050-ESP8266-MicroPython/mpu6050.py
 echo "mpu6050.py loaded"
 ampy -p $1 -b 115200 put ../sensors/mpu.py
 echo "mpu.py loaded"
@@ -17,5 +17,5 @@ ampy -p $1 -b 115200 put ../sensors/dht_sensor.py
 echo "dht_sensor.py loaded"
 ampy -p $1 -b 115200 put ../sensors/co2_sensor.py
 echo "mq135_sensor.py loaded"
-ampy -p $1 -b 115200 put ../MQ135/mq135.py
+ampy -p $1 -b 115200 put ../libraries/MQ135/mq135.py
 echo "mq135.py loaded"
