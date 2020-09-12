4bdeb54ef0fc9924deb8238e2f47c93055517cd1
Kunj Dave <kdav0006@student.monash.edu>
2020-09-12 20:57:26 +1000

    Fix abstract base class

--- wireless_modules/sensors/sensor_base.py
+++ wireless_modules/sensors/sensor_base.py
@@ -1,11 +1,13 @@
-"""
-Abstract base class for all sensors
-"""
+from abc import ABC, abstractmethod
 
 
-class Sensor:
+class Sensor(ABC):
+    """
+    Abstract base class for all sensors
+    """
     def __init__(self):
         pass
 
+    @abstractmethod
     def read(self):
         pass
