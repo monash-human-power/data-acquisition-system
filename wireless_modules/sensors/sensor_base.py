85379d16f9461adbacbaa0549df1538c636702c7
Kunj Dave <kdav0006@student.monash.edu>
2020-09-13 10:32:50 +1000

    Add comment explaining abtract read() method

--- wireless_modules/sensors/sensor_base.py
+++ wireless_modules/sensors/sensor_base.py
@@ -10,4 +10,10 @@ class Sensor(ABC):
 
     @abstractmethod
     def read(self):
+        """
+        Read the relevant sensor values and return an array of dictionaries for each value. Each dictionary should have
+        a "type" key (associated with the name of the measurement taken in String format) and a "value" key (associated
+        with the sensor values - in any format suitable).
+        :return:
+        """
         pass
