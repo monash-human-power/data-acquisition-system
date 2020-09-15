53dd5f41147ae7772d40c7eed0a81ee9e498b449
Kunj Dave <kdav0006@student.monash.edu>
2020-09-15 20:14:01 +1000

    Add more comment

--- wireless_modules/libraries/abc.py
+++ wireless_modules/libraries/abc.py
@@ -1,5 +1,6 @@
 # Sourced from: https://github.com/micropython/micropython-lib/tree/master/abc
-# This method is used with the Python's decorator (@) for tagging abstract methods
+# This method is used with Python's decorator (@) for tagging abstract methods in MicroPython (which doesn't have a
+# built-in ABC module).
 # Note: It is still WIP and so would not enforce that any method tagged with @abstractmethod be implemented in a sub
 # class, but is used nevertheless to convey the intent
 
