f850d4dd835e01fdc63c8163be14bef0d642de79
Blake Haydon <blake@haydon.dev>
2020-10-03 15:09:30 +1000

    Add general exception handling case

--- DAS/das/mqtt_recorder_V2.py
+++ DAS/das/mqtt_recorder_V2.py
@@ -72,5 +72,5 @@ if __name__ == "__main__":
         while True:
             try:
                 pass
-            except KeyboardInterrupt:
+            except (KeyboardInterrupt, Exception):
                 graceful_exit(main_recorder)
