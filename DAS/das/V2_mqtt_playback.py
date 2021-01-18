3ff42197c23c5d7a7b0ed25d00f91cf1cc8c5687
Blake <bhay0010@student.monash.edu>
2021-01-18 15:14:14 +1100

    Add Correct DAS Documentation (#65)
    * Add initial logger_V2
    
    * Add basic logging class
    
    * Add nice recording
    
    * Make sure there are no negative logs
    
    * Add correct ingestion
    
    * Add full playback functionality
    
    * Fix delimiter and comma to ensure that it works with all datatypes
    
    * Add proper command line input for playback
    
    * Add little -s for speed flag
    
    * Add scaled speed as an output for the logging
    
    * Rearange files
    
    * Clean redundant code
    
    * Fix logger command line tool
    
    * Add tests and use async for playback
    
    * Add nice async code
    
    * Change logger to Record to aid in readability
    
    * Add timed input for the logger
    
    * Add correct graceful exit for ctrl c
    
    * Rename logger to recorder
    
    * Add documentation for Record class
    
    * Add dict to store csv config
    
    * Cleanup old syncronous coude
    
    * Add inline comments for reading log file
    
    * Add docs for Playback class
    
    * Update poetry
    
    * Add proper logger
    
    * Add graceful exit function
    
    * Fix order of logging for exit
    
    * Allow for unlimited logs
    
    * Remove old comment
    
    * Move function out of recorder init
    
    * Make non vervose more verbose
    
    * Fix parser.add_argument for broker
    
    * Update poetry
    
    * Add default for argparse
    
    * Fix spelling mistake subscribe
    
    * Add self contained tests
    
    * Black formatting
    
    * Change Record class to Recorder class
    
    * uppercase _CLIENT property
    
    * Add type annotations to return values
    
    * Add start recording method
    
    * Remove double default text
    
    * Update test files to new Recorder class
    
    * Add general exception handling case
    
    * Refactor graceful exit
    
    * Remove old mqtt logger
    
    * Rename playback and recorder scripts
    
    * Update MQTT Recorder docs
    
    * Fix MQTT recorder desc
    
    * Add MQTT Playback docs
    
    * Add <br/> tag to space out
    
    * Rename fake_module_tester to fake_module
    
    * Update -h docs for fake_module
    
    * Add Fake Module docs
    
    * Add testing command
    
    * Add gracefull exit and to playback tool
    
    * Remove non-tests from the tests folder
    
    * Add better error handling and remove self._LOGGER
    
    * Update main readme to link to the DAS folder
    
    * Remove git conflict markers
    
    * Rename old files
    
    * Add full stop
    
    * Add Default Value column to docs
    
    * Add poetry prerequisite
    
    * Update README clone link
    
    * remove ant plus sensors as that has been merged with the das folder
    
    * Update root README with links and correct folders
    
    * Rename mqtt_test and add defaults to -h printout
    
    * Rename files with V3 or V2 label
    
    * Add units to time inputs
    
    * Remove duplicate data words
    
    * Remove old MQTT topics as per  #66
    
    * Remove duplicated file
    
    * Update V2_mqtt_playback.py with correct MHP topics

---
+++ DAS/das/V2_mqtt_playback.py
@@ -0,0 +1,122 @@
+import sys
+import argparse
+import paho.mqtt.client as mqtt
+
+from mhp import topics
+
+from das.utils.das_data_generator import send_csv_data, send_fake_data
+
+# Arguments
+parser = argparse.ArgumentParser(
+    description="DAS MQTT python script",
+    add_help=True,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "-t",
+    "--time",
+    action="store",
+    type=int,
+    default=1,
+    help="length of time to send data",
+)
+parser.add_argument(
+    "-r",
+    "--rate",
+    action="store",
+    type=float,
+    default=0.5,
+    help="rate of data in seconds",
+)
+parser.add_argument(
+    "--host",
+    action="store",
+    type=str,
+    default="localhost",
+    help="address of the MQTT broker",
+)
+parser.add_argument(
+    "--port", action="store", type=int, default=1883, help="port of the MQTT broker"
+)
+parser.add_argument(
+    "--username",
+    action="store",
+    type=str,
+    help="username for the MQTT broker (optional)",
+)
+parser.add_argument(
+    "--password",
+    action="store",
+    type=str,
+    help="password for the MQTT broker (optional)",
+)
+parser.add_argument(
+    "-f",
+    "--file",
+    action="store",
+    type=str,
+    help="The csv file to replay. If not specified, makes up data.",
+)
+parser.add_argument(
+    "-s",
+    "--speed",
+    action="store",
+    type=float,
+    default=1,
+    help="Replay speed multiplier",
+)
+parser.add_argument(
+    "-j",
+    "--jump",
+    action="store",
+    type=int,
+    default=0,
+    help="Starts replaying from a specified time (in seconds)",
+)
+
+
+def start_publishing(client, args):
+    print("start")
+    client.publish(str(topics.BOOST.start))
+    client.publish(str(topics.DAS.start))
+
+    def send_data_func(data):
+        return client.publish(str(topics.DAS.data), data)
+
+    if args.file is None:
+        send_fake_data(send_data_func, args.time, args.rate)
+    else:
+        send_csv_data(send_data_func, args.file, args.jump, speedup=args.speed)
+
+    print("stop")
+    client.publish(str(topics.DAS.stop))
+    client.publish(str(topics.BOOST.stop))
+    sys.exit(0)
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    broker_address = args.host
+    broker_port = args.port
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    broker_username = args.username
+    broker_password = args.password
+    if broker_username is not None:
+        client.username_pw_set(broker_username, broker_password)
+
+    client.connect(broker_address, port=broker_port)
+    start_publishing(client, args)
+
+    client.loop_forever()
