c3ab24a7116994e58dff16d45b2e3f3ca5a94cc0
Christopher Hall <hallgchris@gmail.com>
2021-01-25 13:55:36 +1100

    Track and publish distance traveeled

--- wireless_modules/sensors/reed_sensor.py
+++ wireless_modules/sensors/reed_sensor.py
@@ -3,15 +3,15 @@ import time
 
 from sensor_base import Sensor
 
-
 us_to_s = 1e-6
 
 
 class ReedSensor(Sensor):
     def __init__(self, reed_pin: int, tyre_circumference: float):
+        self.tyre_circumference = tyre_circumference
         self.last_trigger_time = time.ticks_us()
         self.current_speed = 0
-        self.tyre_circumference = tyre_circumference
+        self.distance_travelled = 0
 
         pin = machine.Pin(reed_pin, machine.Pin.IN, machine.Pin.PULL_UP)
         pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.reed_callback)
@@ -25,7 +25,11 @@ class ReedSensor(Sensor):
             return
 
         self.last_trigger_time = now
+        self.distance_travelled += self.tyre_circumference
         self.current_speed = self.tyre_circumference / (diff_us * us_to_s)
 
     def read(self):
-        return [{"type": "reedVelocity", "value": self.current_speed}]
+        return [
+            {"type": "reedVelocity", "value": self.current_speed},
+            {"type": "reedDistance", "value": self.distance_travelled},
+        ]
