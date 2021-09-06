012f3a7b60c5dc224334dac63663005699de8e17
Blake Haydon <blake.a.haydon@gmail.com>
2021-09-06 16:01:49 +1000

    Add echo statements to install script

--- DAS/das/service/install.sh
+++ DAS/das/service/install.sh
@@ -6,9 +6,17 @@ SERVICE_FILE=logger.service
 
 # Create folder for service if it doesn't yet exist
 mkdir -p $SERVICE_DIR
+echo "✓ Successfully created $SERVICE_DIR"
 
 # Copy service files to service directory
 cp $DAS_DIR/service/$SERVICE_FILE $SERVICE_DIR
+echo "✓ Coppied service files to $SERVICE_DIR"
 
 # Append working directory to end of service file
 echo WorkingDirectory=$DAS_DIR >> $SERVICE_DIR/$SERVICE_FILE
+echo "✓ Appended working directory to $SERVICE_DIR"
+
+# Point user towards README for further setup
+echo ""
+echo "- View README.md for more information on how to run the installed service"
+echo ""
