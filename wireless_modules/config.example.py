1e3ddc32e5e12f95c562e38d630553dbf3c91cf2
Christopher Hall <17876556+hallgchris@users.noreply.github.com>
2021-09-18 11:42:27 +1000

    Update example config pins

--- wireless_modules/config.example.py
+++ wireless_modules/config.example.py
@@ -1,10 +1,11 @@
-from machine import Pin
-
 ESSID = "<WIFI NETWORK NAME HERE>"
 PASSWORD = "<WIFI PASSWORD HERE>"
 
 MQTT_BROKER = "<Broker domain address or IP address>"
 
-# The pin number reference to the built-in led
-led_pin = 2
+# Pins for R, G, B channels of status LED
+STATUS_LED_PINS = [14, 12, 13]
+
+# Pin for LED on ESP32 board
+BUILTIN_LED_PIN = 2
 
