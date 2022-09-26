ea0cc463ecc73f85916d98b6da66e4e43b2c538f
Cheuk Lam Winnie Chui <cchu0061@student.monash.edu>
2022-09-26 18:22:33 +1000

    add wind sensor

---
+++ wireless_modules/rpi_sensors/wind_sensor.py
@@ -0,0 +1,78 @@
+from rpi_sensor_base import Sensor
+import serial
+import time
+
+
+class WindSensor(Sensor):
+    """
+    A class for an anemometer connected to a Raspberry Pi via serial port.
+    """
+
+    def __init__(self, port):
+        """
+        Initialise the anemometer to read wind direction and wind speed data.
+        :param port: The serial port that the anemometer is connected to.
+        """
+        self.baudrate = 19200
+        self.sensor = serial.Serial(port, self.baudrate)
+
+        self.readings = []
+
+        # track the last time read() was called
+        self.query_time = 0
+        self.MS_TO_SEC = 1/1000
+        self.NS_TO_MS = 1000000
+    
+
+    def read(self):
+        """
+        Read the wind direction (deg) and wind speed (m/s) measurements from the anemometer.
+        :return: An array of length 2 containing a wind direction dictionary and a wind speed dictionary.
+                 Each dictionary contains a "type" key associated with the sensor type (str) and a "value" key associated with
+                 another dictionary containing key-value pairs of the measurement method (str) and its relevant data (int).
+        """
+        # return previous data if read() was already called less than 1 second ago
+        time_since_last_read = (time.time_ns()*self.NS_TO_MS - self.query_time) * self.MS_TO_SEC
+        if time_since_last_read < 1:
+            return self.readings
+        
+        # read data from anemometer and decode data from bytes to string
+        data_bytes = self.sensor.readline()
+        data = data_bytes.decode('utf-8')
+
+        # extract wind direction (deg) data
+        min_direction = data[7:10]
+        avg_direction = data[15:18]
+        max_direction = data[23:26]
+
+        # extract wind speed (m/s) data
+        min_speed = data[31:34]
+        avg_speed = data[39:42]
+        max_speed = data[47:50]
+
+        # update wind direction and speed data
+        self.readings = [
+            {
+                "type": "windDirection",
+                "value": {
+                    "minDirection": min_direction,
+                    "avgDirection": avg_direction,
+                    "maxDirction": max_direction
+                }
+            },
+            {
+                "type": "windSpeed",
+                "value": {
+                    "minSpeed": min_speed,
+                    "avgSpeed": avg_speed,
+                    "maxSpeed": max_speed
+                }
+            }
+        ]
+
+        # update the las time of read() being called
+        self.query_time = time.time_ns() * self.NS_TO_MS
+
+        # print(self.readings)
+        
+        return self.readings
