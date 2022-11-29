6a9e16697016782cf12cf587c48f10dd2bad3f29
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2022-11-29 17:30:34 +1100

    Reformat conversion code to work within the class

--- DAS/das/log_2_excel.py
+++ DAS/das/log_2_excel.py
@@ -13,132 +13,27 @@ from mhp import topics
 import paho.mqtt.client as mqtt
 
 
-# Load env variables
-load_dotenv()
-
-MQTT_LOG_FILE = os.getenv("MQTT_LOG_FILE")
-EXCEL_LOG_FILE = os.getenv("EXCEL_LOG_FILE")
-
-# Set logging to output all info by default
-logging.basicConfig(
-    format="%(message)s",
-    level=logging.INFO,
-    handlers=[RichHandler()],
-)
-
-
-def parse_module_data(module_id: int, cur: sqlite3.Cursor) -> pd.DataFrame:
-    """Parses the module data if it is from the sensors"""
-
-    query = f""" 
-        SELECT * FROM LOG 
-        WHERE TOPIC='{topics.WirelessModule.id(module_id).data}'
-        """
-
-    # Store all of the flattened data dicts inside a big array before converting to a dataframe
-    data_arr = []
-    for data_row in cur.execute(query).fetchall():
-        # Unbundle SQL row into individual columns
-        (id, run_id, unix_time, topic, message) = data_row
-
-        # Decode binary string messages into json
-        try:
-            utf8_data = message.decode("utf-8")
-            json_data = json.loads(utf8_data)
-        except:
-            logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
-            continue
-
-        # Retrieve sensor data from python dict
-        data_dict = {"unix_time": unix_time, "run_id": run_id}
-        for sensor in json_data["sensors"]:
-            sensor_name = str(module_id) + "_" + sensor["type"]
-            sensor_value = sensor["value"]
-
-            # For nested sensor values
-            if isinstance(sensor_value, dict):
-                for (sub_sensor, sub_sensor_value) in sensor_value.items():
-                    sub_sensor_name = sensor_name + "_" + sub_sensor
-                    data_dict[sub_sensor_name] = sub_sensor_value
-
-            # For single sensor values
-            else:
-                data_dict[sensor_name] = sensor_value
-
-        data_arr.append(data_dict)
-
-    return pd.DataFrame(data_arr)
-
-
-def parse_module_battery(module_id: int, cur: sqlite3.Cursor) -> pd.DataFrame:
-    """Parses the module data if it is from the battery"""
-
-    query = f""" 
-        SELECT * FROM LOG 
-        WHERE TOPIC='{topics.WirelessModule.id(module_id).battery}'
-        """
-
-    # Store all of the data dicts inside a big array before converting to a dataframe
-    data_arr = []
-    for data_row in cur.execute(query).fetchall():
-        # Unbundle SQL row into individual columns
-        (id, run_id, unix_time, topic, message) = data_row
-
-        # Decode binary string messages into json
-        try:
-            utf8_data = message.decode("utf-8")
-            json_data = json.loads(utf8_data)
-        except:
-            logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
-            continue
-
-        data_arr.append(
-            {
-                "unix_time": unix_time,
-                "run_id": run_id,
-                f"{module_id}_voltage": json_data["voltage"],
-            }
-        )
-
-    return pd.DataFrame(data_arr)
-
-
-def parse_all_raw(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """Convert all data in the LOG table into a single dataframe"""
-
-    # Store all of the data dicts inside a big array before converting to a dataframe
-    data_arr = []
-    for data_row in cur.execute("SELECT * FROM LOG").fetchall():
-        # Unbundle SQL row into individual columns
-        (id, run_id, unix_time, topic, message) = data_row
-
-        # Decode the data from a binary string to utf-8
-        try:
-            data_arr.append(
-                {
-                    "id": id,
-                    "topic": topic,
-                    "unix_time": unix_time,
-                    "run_id": run_id,
-                    "data": message.decode("utf-8"),
-                }
-            )
-        except:
-            logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
-            continue
-
-    return pd.DataFrame(data_arr)
-
-# Create a class that on start and stop commands will start and stop logging
-# Need to connect to mqtt Service, requires mqtt imports
-
 class DataLogger:
 
-    def __init__(self, broker_ip, port=1883) -> None:
+    def __init__(self, broker_ip='localhost', port=1883) -> None:
         self.v3_start = topics.V3.start
         self.broker_ip = broker_ip
         self.port = port
         self.mqtt_client = None
+
+        #Data for log conversions
+        # Load env variables
+        load_dotenv()
+
+        self.MQTT_LOG_FILE = os.getenv("MQTT_LOG_FILE")
+        self.EXCEL_LOG_FILE = os.getenv("EXCEL_LOG_FILE")
+
+        # Set logging to output all info by default
+        logging.basicConfig(
+            format="%(message)s",
+            level=logging.INFO,
+            handlers=[RichHandler()],
+        )
 
     
     def on_connect(self, client, userdata, flags, rc):
@@ -171,44 +66,144 @@ class DataLogger:
         self.mqtt_client.loop_start()
         while True:
             time.sleep(1)
+    
+    #Functions for log conversion
+    def parse_module_data(self, module_id: int, cur: sqlite3.Cursor) -> pd.DataFrame:
+        """Parses the module data if it is from the sensors"""
+
+        query = f""" 
+            SELECT * FROM LOG 
+            WHERE TOPIC='{topics.WirelessModule.id(module_id).data}'
+            """
+
+        # Store all of the flattened data dicts inside a big array before converting to a dataframe
+        data_arr = []
+        for data_row in cur.execute(query).fetchall():
+            # Unbundle SQL row into individual columns
+            (id, run_id, unix_time, topic, message) = data_row
+
+            # Decode binary string messages into json
+            try:
+                utf8_data = message.decode("utf-8")
+                json_data = json.loads(utf8_data)
+            except:
+                logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
+                continue
+
+            # Retrieve sensor data from python dict
+            data_dict = {"unix_time": unix_time, "run_id": run_id}
+            for sensor in json_data["sensors"]:
+                sensor_name = str(module_id) + "_" + sensor["type"]
+                sensor_value = sensor["value"]
+
+                # For nested sensor values
+                if isinstance(sensor_value, dict):
+                    for (sub_sensor, sub_sensor_value) in sensor_value.items():
+                        sub_sensor_name = sensor_name + "_" + sub_sensor
+                        data_dict[sub_sensor_name] = sub_sensor_value
+
+                # For single sensor values
+                else:
+                    data_dict[sensor_name] = sensor_value
+
+            data_arr.append(data_dict)
+
+        return pd.DataFrame(data_arr)
+    
+    def parse_module_battery(self, module_id: int, cur: sqlite3.Cursor) -> pd.DataFrame:
+        """Parses the module data if it is from the battery"""
+
+        query = f""" 
+            SELECT * FROM LOG 
+            WHERE TOPIC='{topics.WirelessModule.id(module_id).battery}'
+            """
+
+        # Store all of the data dicts inside a big array before converting to a dataframe
+        data_arr = []
+        for data_row in cur.execute(query).fetchall():
+            # Unbundle SQL row into individual columns
+            (id, run_id, unix_time, topic, message) = data_row
+
+            # Decode binary string messages into json
+            try:
+                utf8_data = message.decode("utf-8")
+                json_data = json.loads(utf8_data)
+            except:
+                logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
+                continue
+
+            data_arr.append(
+                {
+                    "unix_time": unix_time,
+                    "run_id": run_id,
+                    f"{module_id}_voltage": json_data["voltage"],
+                }
+            )
+
+        return pd.DataFrame(data_arr)
+
+    def parse_all_raw(self, cur: sqlite3.Cursor) -> pd.DataFrame:
+        """Convert all data in the LOG table into a single dataframe"""
+
+        # Store all of the data dicts inside a big array before converting to a dataframe
+        data_arr = []
+        for data_row in cur.execute("SELECT * FROM LOG").fetchall():
+            # Unbundle SQL row into individual columns
+            (id, run_id, unix_time, topic, message) = data_row
+
+            # Decode the data from a binary string to utf-8
+            try:
+                data_arr.append(
+                    {
+                        "id": id,
+                        "topic": topic,
+                        "unix_time": unix_time,
+                        "run_id": run_id,
+                        "data": message.decode("utf-8"),
+                    }
+                )
+            except:
+                logging.error(f"Message not utf-8 encoded. Skipping id:{id}.")
+                continue
+
+        return pd.DataFrame(data_arr)
+    
+    def convertXL(self):
+        # Connect to the sqlite database that has all of the MQTT logs
+        con = sqlite3.connect(self.MQTT_LOG_FILE)
+        cur = con.cursor()
+
+        with pd.ExcelWriter(self.EXCEL_LOG_FILE, engine="xlsxwriter") as writer:
+            for module_id in [1, 2, 3, 4]:
+                module_data = self.parse_module_data(module_id, cur)
+                module_battery = self.parse_module_battery(module_id, cur)
+
+                if not module_data.empty:
+                    logging.info(f"Exporting wireless module {module_id} sensor data")
+                    module_data.to_excel(
+                        writer,
+                        sheet_name=f"module_{module_id}_data",
+                        index=False,
+                    )
+
+                if not module_battery.empty:
+                    logging.info(f"Exporting wireless module {module_id} battery data")
+                    module_battery.to_excel(
+                        writer,
+                        sheet_name=f"module_{module_id}_battery",
+                        index=False,
+                    )
+
+            logging.info(f"Exporting all data to excel")
+            self.parse_all_raw(cur).to_excel(writer, sheet_name="raw_data", index=False)
+
+        con.close()
+
+
+    
+
 
 if __name__ == "__main__":
-    # # Connect to the sqlite database that has all of the MQTT logs
-    # con = sqlite3.connect(MQTT_LOG_FILE)
-    # cur = con.cursor()
-
-    # with pd.ExcelWriter(EXCEL_LOG_FILE, engine="xlsxwriter") as writer:
-    #     for module_id in [1, 2, 3, 4]:
-    #         module_data = parse_module_data(module_id, cur)
-    #         module_battery = parse_module_battery(module_id, cur)
-
-    #         if not module_data.empty:
-    #             logging.info(f"Exporting wireless module {module_id} sensor data")
-    #             module_data.to_excel(
-    #                 writer,
-    #                 sheet_name=f"module_{module_id}_data",
-    #                 index=False,
-    #             )
-
-    #         if not module_battery.empty:
-    #             logging.info(f"Exporting wireless module {module_id} battery data")
-    #             module_battery.to_excel(
-    #                 writer,
-    #                 sheet_name=f"module_{module_id}_battery",
-    #                 index=False,
-    #             )
-
-    #     logging.info(f"Exporting all data to excel")
-    #     parse_all_raw(cur).to_excel(writer, sheet_name="raw_data", index=False)
-
-    # con.close()
-
-    #Need to change the IP address
-    BROKER_IP = "20.243.33.45" #create a config file?
-    data_logger = DataLogger('localhost')
-
-    #start
+    
+    data_logger = DataLogger()
     data_logger.start()
-
-
-        
