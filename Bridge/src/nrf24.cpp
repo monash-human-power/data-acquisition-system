7c78c3955198969174041bedf8b6c8805a255bbc
mhp <mhp>
2023-02-03 19:35:25 +1100

    Update radio config

--- Bridge/src/nrf24.cpp
+++ Bridge/src/nrf24.cpp
@@ -12,13 +12,14 @@ Nrf24Radio::Nrf24Radio(bool is_bike, uint16_t ce_pin, uint16_t cs_pin)
     std::cout << "Starting NRF24 Radio..." << std::endl;
 
     // TODO: Test with RF24_SPI_SPEED, or at least something faster
-    this->nrf24_ = RF24(ce_pin, cs_pin, 100000);
+    this->nrf24_ = RF24(ce_pin, cs_pin, 1'000'000);
 
     if (!this->nrf24_.begin())
         throw "NRF24 begin failed. Check wiring?";
 
     this->nrf24_.setPayloadSize(PACKET_LENGTH);
-    this->nrf24_.setPALevel(RF24_PA_MAX);
+    this->nrf24_.setPALevel(RF24_PA_HIGH);
+    this->nrf24_.setDataRate(RF24_250KBPS);
     this->nrf24_.openWritingPipe(addresses[is_bike]);
     this->nrf24_.openReadingPipe(1, addresses[!is_bike]);
 
