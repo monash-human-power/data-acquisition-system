2dfd73111fd713751e42129b345e6d89e4cdb8e6
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2023-02-12 22:26:30 +1100

    Adding install code for log_2_excel script

--- services/install.sh
+++ services/install.sh
@@ -6,6 +6,7 @@ DAS_DIR=$(dirname $(cd $(dirname "${BASH_SOURCE[0]}") && pwd))
 LOGGER_SERVICE_FILE=logger.service
 ANT_PLUS_SERVICE_FILE=ant-plus.service
 CRASH_ALERT_SERVICE_FILE=crash_alert.service
+LOG_TO_EXCEL_SERVICE_FILE=log-to-excel.service
 
 # Create folder for service if it doesn't yet exist
 mkdir -p $SERVICE_DIR
@@ -15,12 +16,14 @@ echo "✓ Successfully created $SERVICE_DIR"
 cp $DAS_DIR/services/$LOGGER_SERVICE_FILE $SERVICE_DIR
 cp $DAS_DIR/services/$ANT_PLUS_SERVICE_FILE $SERVICE_DIR
 cp $DAS_DIR/services/$CRASH_ALERT_SERVICE_FILE $SERVICE_DIR
+cp $DAS_DIR/services/$LOG_TO_EXCEL_SERVICE_FILE $SERVICE_DIR
 echo "✓ Coppied service files to $SERVICE_DIR"
 
 # Append working directory to end of service file
 echo WorkingDirectory=$DAS_DIR/DAS/das >> $SERVICE_DIR/$LOGGER_SERVICE_FILE
 echo WorkingDirectory=$DAS_DIR/ant_plus_sensor >> $SERVICE_DIR/$ANT_PLUS_SERVICE_FILE
 echo WorkingDirectory=$DAS_DIR/crash_alert >> $SERVICE_DIR/$CRASH_ALERT_SERVICE_FILE
+echo WorkingDirectory=$DAS_DIR/DAS/das >> $SERVICE_DIR/$LOG_TO_EXCEL_SERVICE_FILE
 echo "✓ Appended working directory to $SERVICE_DIR"
 
 # Point user towards README for further setup
