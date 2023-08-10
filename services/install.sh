765419784ba531e7c9ccad554c206c9daff0e70d
Jotham Gates <jgat0002@student.monash.edu>
2023-08-10 18:48:18 +1000

    Added service files for radio bridge

--- services/install.sh
+++ services/install.sh
@@ -5,6 +5,8 @@ DAS_DIR=$(dirname $(cd $(dirname "${BASH_SOURCE[0]}") && pwd))
 
 LOGGER_SERVICE_FILE=logger.service
 ANT_PLUS_SERVICE_FILE=ant-plus.service
+CHASE_TX_SERVICE_FILE=chase_tx_mqtt.service
+CHASE_RX_SERVICE_FILE=chase_rx_mqtt.service
 
 # Create folder for service if it doesn't yet exist
 mkdir -p $SERVICE_DIR
@@ -13,11 +15,15 @@ echo "✓ Successfully created $SERVICE_DIR"
 # Copy service files to service directory
 cp $DAS_DIR/services/$LOGGER_SERVICE_FILE $SERVICE_DIR
 cp $DAS_DIR/services/$ANT_PLUS_SERVICE_FILE $SERVICE_DIR
+cp $DAS_DIR/services/$CHASE_TX_SERVICE_FILE $SERVICE_DIR
+cp $DAS_DIR/services/$CHASE_RX_SERVICE_FILE $SERVICE_DIR
 echo "✓ Coppied service files to $SERVICE_DIR"
 
 # Append working directory to end of service file
 echo WorkingDirectory=$DAS_DIR/DAS/das >> $SERVICE_DIR/$LOGGER_SERVICE_FILE
 echo WorkingDirectory=$DAS_DIR/ant_plus_sensor >> $SERVICE_DIR/$ANT_PLUS_SERVICE_FILE
+echo WorkingDirectory=$DAS_DIR/Bridge >> $SERVICE_DIR/$CHASE_TX_SERVICE_FILE
+echo WorkingDirectory=$DAS_DIR/Bridge >> $SERVICE_DIR/$CHASE_RX_SERVICE_FILE
 echo "✓ Appended working directory to $SERVICE_DIR"
 
 # Point user towards README for further setup
