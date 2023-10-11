937801e3b4abc6fe43d9339deccebe5997e5cc0d
Ammanuell <ammanuelllll@gmail.com>
2023-10-11 18:40:34 +1100

    Added MQTT posting to Bump strips and Lap timer

--- lap_timer/LapTimer.py
+++ lap_timer/LapTimer.py
@@ -11,20 +11,23 @@ class LapTimer():
         self.last_lap_time = None
         self.last_distance = None
         self.last_lap_distance = None
+        self.lap_number = 1
 
     
         self.ant_topic = b"/v3/wireless_module/4/data"
         self.lap_topic = b"Bla Bla"
-        self.broker_id = "Bla Bla"
+        self.lap_data_topic = b"Bla Bla Bla"
+        self.topics = [self.ant_topic, self.lap_topic]
 
+
+        self.broker_id = "Bla Bla"
         client_id = "Ammanuel"
         self.mqtt = client.Client(client_id)
         pass # Set last will
 
+
         
-        # Don't know if this is where I am supposed to connect
-        topics = [self.ant_topic, self.lap_topic]
-        self.connect_to_mqtt(self.broker_id, topics)
+        self.connect_to_mqtt(self.broker_id)
 
         
     
@@ -34,9 +37,18 @@ class LapTimer():
             lap_distance = self.last_distance - self.last_lap_distance
             lap_speed = lap_distance/lap_time
             # Do something to communicate time, speed, and distance to DAShboard
+            lap_data = {
+                "lapNumber": self.lap_number,
+                "time": lap_time,
+                "distance": lap_distance,
+                "speed": lap_speed,
+
+            }
+            self.mqtt.publish(self.lap_data_topic, json.dumps(lap_data))
 
             self.last_lap_distance = self.last_distance
             self.last_lap_time = time.time()
+            self.lap_number += 1 
         else:
             self.last_lap_time = time.time()
     
@@ -50,12 +62,18 @@ class LapTimer():
             msg_data = json.loads(str(message.payload.decode("utf-8")))
             self.last_lap_distance = msg_data["antDistance"]
 
-    def connect_to_mqtt(self, broker_id, topics):
-        self.mqtt.on_message = self.on_message
-
-        self.mqtt.connect(broker_id)
-
-        for topic in topics:
+    def on_connect(self, client, userdata, flags, connection_result):
+        for topic in self.topics:
             self.mqtt.subscribe(topic, qos=1)
 
+
+    
+    def connect_to_mqtt(self, broker_id):
+        self.mqtt.on_message = self.on_message
+        self.mqtt.on_connect = self.on_connect
+
+        self.mqtt.connect(broker_id)
+        self.mqtt.loop_forever(retry_first_connection = True)
+
+        
         
--- lap_timer/bumpstrips/ArduinoToMQTT.py
+++ lap_timer/bumpstrips/ArduinoToMQTT.py
@@ -3,9 +3,10 @@ import time
 #from paho.mqtt import client
 
 ser = serial.Serial('COM3', 9600)
+print("Arduino connected...")
 time.sleep(1)
 # MQTT Setup
-"""
+
 
 client_id = "Ammanuel" # 
 broker_id = 3 # change
@@ -19,24 +20,25 @@ mqtt = client.Client(client_id)
 
 connected = False
 while not connected:
-        print("Connectiing...")
+        print("Connecting to MQTT...")
         try:
             mqtt.connect(broker_id)
+            print("MQTT connected...")
             connected = True
         except:
             print("Failed to connect. Retrying...")
             time.sleep(1)
 
-"""
-print("Arduino connected...")
+
+
 while True:
     message = ser.readline().decode().strip()
     if message:
         print(message) 
-        """
+    
         if message == start_message: 
             mqtt.publish(topic_string_1, message)
         elif message == lap_message:
             mqtt.publish(topic_string_2, message)
-        """
+        
 
