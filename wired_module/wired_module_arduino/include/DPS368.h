9f9987b092144a3d8cc98cd5b26522a56e568958
Joseph <129715233+Jooi025@users.noreply.github.com>
2025-02-21 00:21:51 +1100

    added more function to the DPS358 class

--- wired_module/wired_module_arduino/include/DPS368.h
+++ wired_module/wired_module_arduino/include/DPS368.h
@@ -3,6 +3,7 @@
 #ifndef DPS368_H
 #define DPS368_H
 
+// header guard
 #define DPS368_ADDRESS (0x76 << 1) 
 #define DPS368_ID 0x10
 
@@ -26,6 +27,22 @@
 #define DPS368_RESERVED    0x22  // Starts at 0x22, spans multiple addresses
 #define DPS368_COEF_SRCE   0x28
 
+
+/**
+ * From the section 8.5 of the datasheet (Sensor Operating Mode and Status) 
+ * Set measurement mode and type:
+    Standby Mode
+    000 - Idle / Stop background measurement
+    Command Mode
+    001 - Pressure measurement
+    010 - Temperature measurement
+    011 - na.
+    100 - na.
+    Background Mode
+    101 - Continous pressure measurement
+    110 - Continous temperature measurement
+    111 - Continous pressure and temperature measurement
+ */
 enum DPS368Mode {
     IDLE = 0x00,
     CMD_PRS = 0x01,
@@ -47,6 +64,14 @@ class DPS368:public I2cSensorBase {
     // Methods
     void configure() override;
     void read() override;
+
+    boolean is_coef_ready();
+    boolean is_sensor_ready();
+
+    void get_coefficient();
+    
+    void select_mode(DPS368Mode measurementMode);
+
 };
 
 #endif // DPS368_H
--- wired_module/wired_module_arduino/src/DPS368.cpp
+++ wired_module/wired_module_arduino/src/DPS368.cpp
@@ -1,9 +1,54 @@
 #include <DPS368.h>
 
+/*
+Check if the DSP368's coefficient register is available. (Max 40ms - Section 3.6 of the Datasheet)
+TODO: in the future the is_coef_ready() and is_sensor_ready() could be combine since they are 
+reading the same register */
+boolean DPS368::is_coef_ready(){
+    read_sensor_register(DPS368_MEAS_CFG,1,1000);
+    // Bit 7 of the MEAS_CFG register is the COEF_RDY
+    return (this->readBuffer[0] & 0x128);
+}
+
+/*
+Check if the DPS368's preasure sensor is completely initialised. (Max 12ms - Section 3.6 of the Datasheet)
+It is recommend not to start measurements until the sensor has
+completed the self intialisation.*/ 
+boolean DPS368::is_sensor_ready(){
+    read_sensor_register(DPS368_MEAS_CFG,1,1000);
+
+    // Bit 6 of the MEAS_CFG register is the SENSOR_RDY
+    return (this->readBuffer[0] & 0x64);
+}
+
+// TODO: 
+void DPS368::get_coefficient(){
+
+}
+
+void DPS368::select_mode(DPS368Mode measurementMode){
+    write_sensor_register(DPS368_MEAS_CFG,(int) measurementMode,1000);
+}
+
 void DPS368::configure(){
+
+    // Check product ID to validate if the sensor is connected to I2C
     read_sensor_register(DPS368_PRODUCT_ID, 1,1000);
-    if (this->readBuffer[0] == DPS368_ID) printf("DPS368 found\n");
-    else printf("DPS368 not found\n");
+    if (this->readBuffer[0] == DPS368_ID){
+        printf("DPS368 found\n");
+    } else {
+        printf("DPS368 not found\n");
+        return ;
+    }
+
+    // blocks until the sensor is initialised and coefficient is available
+    while(!this->is_sensor_ready());
+    while(!this->is_coef_ready()); 
+    
+    // Set oversampling 
+    // Set operating mode 
+    this->select_mode(CON_BOTH);
+        
     
     
 }
