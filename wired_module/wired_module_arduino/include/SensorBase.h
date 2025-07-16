950ac08405c3b0c760276ce15bf59834ac2fb7e4
Arnav Sethi <aset0018@student.monash.edu>
2025-07-17 03:24:33 +0530

    fixed sensorBase to publish data to topics

--- wired_module/wired_module_arduino/include/SensorBase.h
+++ wired_module/wired_module_arduino/include/SensorBase.h
@@ -1,34 +1,33 @@
-#include <CAN.h>
-#include <ArduinoJson.h>  // Include ArduinoJson for JSON handling
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #ifndef SENSOR_BASE
 #define SENSOR_BASE
 
-// Abstract Class
+extern PubSubClient mqttClient;  // Declare external MQTT client
+
 class SensorBase {
 public:
     uint8_t sensorID;
 
-    // Abstract methods
     virtual void configure() = 0;
     virtual void read() = 0;
 
-    // Method to send the sensor data in JSON format
     void send() {
         String json = generateJson();        // Generate the JSON string
-        sendJson(sensorID, json);            // Send the JSON data over CAN
+        sendJson(sensorID, json);            // Send via MQTT
     }
 
 protected:
-    // Pure virtual function for generating JSON data in derived classes
     virtual String generateJson() = 0;
 
-    // Function to send JSON data over CAN
     void sendJson(uint8_t sensorID, String json) {
-        CAN.beginPacket(sensorID);
-        CAN.write(json.c_str(), json.length());  // Write JSON string to CAN packet
-        CAN.endPacket();
+        char topic[32];
+        snprintf(topic, sizeof(topic), "sensor/%02x", sensorID);
+        mqttClient.publish(topic, json.c_str());
     }
 };
 
 #endif
+
